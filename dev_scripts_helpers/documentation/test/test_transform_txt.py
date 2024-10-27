7b20ce83b67d1dcfb93d43c5e69b2f8752f42702
GP Saggese <saggese@gmail.com>
2024-10-27 18:11:24 -0400

    Improve

--- dev_scripts_helpers/documentation/test/test_transform_txt.py
+++ dev_scripts_helpers/documentation/test/test_transform_txt.py
@@ -1,9 +1,8 @@
 import logging
-import os
-from typing import List
+import pprint
 
 import dev_scripts_helpers.documentation.transform_txt as dsdoremd
-import helpers.hio as hio
+import helpers.hprint as hprint
 import helpers.hunit_test as hunitest
 
 _LOG = logging.getLogger(__name__)
@@ -12,73 +11,130 @@ _LOG = logging.getLogger(__name__)
 # #############################################################################
 
 
-class Test_markdown_to_latex1(hunitest.TestCase):
+class Test_MarkdownItem1(hunitest.TestCase):
+    
+    def test_str1(self):
+        item = dsdoremd.MarkdownItem("Title", [
+            dsdoremd.MarkdownItem("Subitem 1", [
+                dsdoremd.MarkdownItem("Subsubitem 1.1")])])
+        act = str(item)
+        exp = r"""
+        * Title
+          * Subitem 1
+            * Subsubitem 1.1
+        """
+        self.assert_equal(act, exp, dedent=True)
 
-    def test_simple_nested_list(self):
+    def test_str2(self):
+        item = dsdoremd.MarkdownItem("Title",
+              [dsdoremd.MarkdownItem("Item 1",
+                     [
+                         dsdoremd.MarkdownItem("Subitem 1")
+                     ]),
+               dsdoremd.MarkdownItem("Item 2")
+               ])
+        act = str(item)
+        exp = r"""
+        * Title
+          * Item 1
+            * Subitem 1
+          * Item 2
+        """
+        self.assert_equal(act, exp, dedent=True)
+
+    def test_MarkdownItems_to_str1(self):
+        item1 = dsdoremd.MarkdownItem("Title", [
+            dsdoremd.MarkdownItem("Subitem 1", [
+                dsdoremd.MarkdownItem("Subsubitem 1.1")])])
+        item2 = dsdoremd.MarkdownItem("Title2", [
+            dsdoremd.MarkdownItem("Subitem 1")])
+        items = [item1, item2]
+        #
+        act = dsdoremd.MarkdownItems_to_str(items)
+        exp = r"""
+        * Title
+          * Subitem 1
+            * Subsubitem 1.1
+        * Title2
+          * Subitem 1"""
+        self.assert_equal(act, exp, dedent=True)
+
+
+class Test_parse_markdown1(hunitest.TestCase):
+
+    def _check(self, markdown: str, exp: str) -> None:
+        """
+        Check the markdown to latex transformation.
+        """
+        markdown = hprint.dedent(markdown)
+        markdown = markdown.split("\n")
+        act = dsdoremd._parse_markdown(markdown)
+        print(act)
+        #
+        act = dsdoremd.MarkdownItems_to_str(act)
+        self.assert_equal(act, exp)
+
+    def test1(self):
         """
         Test a simple nested list with no frame title.
         """
-        markdown = [
-            "* Item 1",
-            "  * Subitem 1.1",
-            "  * Subitem 1.2",
-            "* Item 2"
-        ]
-        expected_output = (
-            "\\begin{itemize}\n"
-            "  \\item Item 1\n"
-            "    \\item Subitem 1.1\n"
-            "    \\item Subitem 1.2\n"
-            "  \\item Item 2\n"
-            "\\end{itemize}\n"
-        )
-        self.assertEqual(markdown_to_latex(markdown), expected_output)
-
-    def test_list_with_frame_title(self):
-        """Test a nested list that includes a frame title."""
-        markdown = [
-            "* Title of Frame",
-            "  * Item 1",
-            "    * Subitem 1.1",
-            "  * Item 2"
-        ]
-        expected_output = (
-            "\\begin{frame}{Title of Frame}\n"
-            "\\begin{itemize}\n"
-            "  \\item Title of Frame\n"
-            "    \\item Item 1\n"
-            "      \\item Subitem 1.1\n"
-            "    \\item Item 2\n"
-            "\\end{itemize}\n"
-            "\\end{frame}\n"
-        )
-        self.assertEqual(markdown_to_latex(markdown), expected_output)
-
-    def test_deeply_nested_list(self):
-        """Test a deeply nested list structure."""
-        markdown = [
-            "* Level 1",
-            "  * Level 2",
-            "    * Level 3",
-            "      * Level 4"
-        ]
-        expected_output = (
-            "\\begin{itemize}\n"
-            "  \\item Level 1\n"
-            "    \\item Level 2\n"
-            "      \\item Level 3\n"
-            "        \\item Level 4\n"
-            "\\end{itemize}\n"
-        )
-        self.assertEqual(markdown_to_latex(markdown), expected_output)
-
-    def test1(self) -> None:
+        markdown = """
+        * Item 1
+          * Subitem 1.1
+          * Subitem 1.2
+        * Item 2
         """
-        Check output dir and file names correctness for absolute destination
-        path.
+        exp = """
         """
-        # Example Usage:
-        markdown_text = """
+        # Run the test.
+        self._check(markdown, exp)
+
+    def test2(self):
+        """
+        Test a nested list that includes a frame title.
+        """
+        markdown = """
+        * Title of Frame
+          * Item 1
+            * Subitem 1.1
+          * Item 2
+        """
+        exp = """
+        \\begin{frame}{Title of Frame}
+        \\begin{itemize}
+          \\item Title of Frame
+            \\item Item 1
+              \\item Subitem 1.1
+            \\item Item 2
+        \\end{itemize}
+        \\end{frame}
+        """
+        # Run the test.
+        self._check(markdown, exp)
+
+    def test3(self):
+        """
+        Test a deeply nested list structure.
+        """
+        markdown = """
+        * Level 1
+          * Level 2
+            * Level 3
+              * Level 4
+        """
+        exp = """
+        \\begin{itemize}
+          \\item Level 1
+            \\item Level 2
+              \\item Level 3
+                \\item Level 4
+        \\end{itemize>
+        """
+        # Run the test.
+        self._check(markdown, exp)
+
+    def test4(self) -> None:
+        markdown = """
         * Title of Frame
         - Item 1
           - Subitem 1.1
@@ -88,8 +144,116 @@ class Test_markdown_to_latex1(hunitest.TestCase):
           1. Ordered Subitem 2.1
           2. Ordered Subitem 2.2
         """
+        exp = (
+            "\\begin{itemize}\n"
+            "  \\item Level 1\n"
+            "    \\item Level 2\n"
+            "      \\item Level 3\n"
+            "        \\item Level 4\n"
+            "\\end{itemize}\n"
+        )
+        # Run the test.
+        self._check(markdown, exp)
 
-        latex_text = dsdoremd.markdown_to_latex(markdown_text)
-        print(latex_text)
+# #############################################################################
 
 
+class Test_markdown_to_latex1(hunitest.TestCase):
+
+    def _check(self, markdown: str, exp: str) -> None:
+        """
+        Check the markdown to latex transformation.
+        """
+        markdown = hprint.dedent(markdown)
+        act = dsdoremd.markdown_to_latex(markdown)
+        exp = hprint.dedent(exp)
+        self.assert_equal(act, exp)
+
+    def test_simple_nested_list(self):
+        """
+        Test a simple nested list with no frame title.
+        """
+        markdown = """
+        * Item 1
+          * Subitem 1.1
+          * Subitem 1.2
+        * Item 2
+        """
+        exp = """
+        \\begin{itemize}
+          \\item Item 1
+            \\begin{itemize}
+              \\item Subitem 1.1
+              \\item Subitem 1.2
+            \\end{itemize}
+          \\item Item 2
+        \\end{itemize}
+        """
+        # Run the test.
+        self._check(markdown, exp)
+
+    def test_list_with_frame_title(self):
+        """
+        Test a nested list that includes a frame title.
+        """
+        markdown = """
+        * Title of Frame
+          * Item 1
+            * Subitem 1.1
+          * Item 2
+        """
+        exp = """
+        \\begin{frame}{Title of Frame}
+        \\begin{itemize}
+          \\item Title of Frame
+            \\item Item 1
+              \\item Subitem 1.1
+            \\item Item 2
+        \\end{itemize}
+        \\end{frame}
+        """
+        # Run the test.
+        self._check(markdown, exp)
+
+    def test_deeply_nested_list(self):
+        """
+        Test a deeply nested list structure.
+        """
+        markdown = """
+        * Level 1
+          * Level 2
+            * Level 3
+              * Level 4
+        """
+        exp = """
+        \\begin{itemize}
+          \\item Level 1
+            \\item Level 2
+              \\item Level 3
+                \\item Level 4
+        \\end{itemize>
+        """
+        # Run the test.
+        self._check(markdown, exp)
+
+    def test1(self) -> None:
+        markdown = """
+        * Title of Frame
+        - Item 1
+          - Subitem 1.1
+            - Subitem 1.1.1
+          - Subitem 1.2
+        - Item 2
+          1. Ordered Subitem 2.1
+          2. Ordered Subitem 2.2
+        """
+        exp = (
+            "\\begin{itemize}\n"
+            "  \\item Level 1\n"
+            "    \\item Level 2\n"
+            "      \\item Level 3\n"
+            "        \\item Level 4\n"
+            "\\end{itemize}\n"
+        )
+        # Run the test.
+        self._check(markdown, exp)
--- dev_scripts_helpers/documentation/transform_txt.py
+++ dev_scripts_helpers/documentation/transform_txt.py
@@ -162,7 +162,7 @@ def increase_chapter(in_file_name, out_file_name):
 
 
 from typing import List, Dict
-
+import pprint
 
 # Define data structure for nested items.
 class MarkdownItem:
@@ -170,18 +170,65 @@ class MarkdownItem:
         self.text = text
         self.children = children or []
 
+    def __str__(self, depth: int = 0) -> str:
+        """
+        Recursively convert a MarkdownItem structure to a formatted markdown string.
 
-def parse_markdown(markdown: List[str], depth: int = 0) -> List[MarkdownItem]:
+        :param item: The MarkdownItem object to convert.
+        :param depth: The current depth level for indentation.
+        :return: A string representing the nested MarkdownItem structure.
+
+        Example:
+        item = MarkdownItem("Title",
+                [MarkdownItem("Subitem 1",
+                    [MarkdownItem("Subsubitem 1.1")])])
+        print(markdown_item_to_string(item))
+            * Title
+              * Subitem 1
+                * Subsubitem 1.1
+        """
+        # Initialize the string with the current item's text and appropriate
+        # indentation.
+        result = "  " * depth + f"* {self.text}\n"
+        # Recursively add each child item's string representation.
+        for child in self.children:
+            result += child.__str__(depth + 1) + "\n"
+        result = result.rstrip()
+        return result
+
+
+def MarkdownItems_to_str(items: List[MarkdownItem]) -> str:
+    hdbg.dassert_isinstance(items, list)
+    txts = []
+    for item in items:
+        hdbg.dassert_isinstance(item, MarkdownItem)
+        txts.append(str(item))
+    txt = "\n".join(txts)
+    return txt
+
+
+def _parse_markdown(markdown: List[str], depth: int = 0) -> List[MarkdownItem]:
     """
-    Recursively parse a list of markdown lines into a nested structure of MarkdownItems.
+    Recursively parse a list of markdown lines into a nested structure of
+    MarkdownItems.
 
     :param markdown: A list of markdown lines.
     :param depth: Current depth level for nested items.
     :return: A list of MarkdownItem instances representing the nested list structure.
 
     Example:
-        >>> parse_markdown(["* Title", "  * Item 1", "    * Subitem 1", "* Item 2"])
-        [MarkdownItem("Title", [MarkdownItem("Item 1", [MarkdownItem("Subitem 1")]), MarkdownItem("Item 2")])]
+        >>> _parse_markdown([
+            "* Title",
+            "  * Item 1",
+            "    * Subitem 1",
+            "  * Item 2"])
+        [MarkdownItem("Title",
+            [MarkdownItem("Item 1", [
+                MarkdownItem("Subitem 1")
+            ]),
+            MarkdownItem("Item 2")
+            ])
+        ]
     """
     # Initialize list to hold parsed items at the current depth.
     items = []
@@ -195,7 +242,7 @@ def parse_markdown(markdown: List[str], depth: int = 0) -> List[MarkdownItem]:
             current_item = MarkdownItem(text=line[1:].strip())
             # Recursively parse children if the next line has more indentation.
             if markdown and markdown[0].startswith(" " * (depth + 2) + "*"):
-                current_item.children = parse_markdown(markdown, depth + 2)
+                current_item.children = _parse_markdown(markdown, depth + 2)
             # Append the item to the current level's list.
             items.append(current_item)
         elif line.startswith(" " * (depth - 2) + "*"):
@@ -215,10 +262,6 @@ def render_latex(items: List[MarkdownItem], depth: int = 0,
     :param depth: Current depth level for LaTeX itemization.
     :param frame_title: Title of the frame if applicable.
     :return: A LaTeX formatted string for the nested list.
-
-    Example:
-        >>> render_latex([MarkdownItem("Title", [MarkdownItem("Item 1", [MarkdownItem("Subitem 1")])])], frame_title="Example Frame")
-        "\\begin{frame}{Example Frame}\n\\begin{itemize}\n  \\item Title\n    \\item Item 1\n      \\item Subitem 1\n\\end{itemize}\n\\end{frame}\n"
     """
     # Base LaTeX block with or without frame depending on depth and frame_title.
     if depth == 0 and frame_title:
@@ -247,7 +290,7 @@ def render_latex(items: List[MarkdownItem], depth: int = 0,
     return latex
 
 
-def markdown_to_latex(markdown: List[str]) -> str:
+def markdown_to_latex(markdown: str) -> str:
     """
     Convert a nested markdown list into LaTeX format, wrapped in a frame if the list starts with "*".
 
@@ -255,18 +298,32 @@ def markdown_to_latex(markdown: List[str]) -> str:
     :return: LaTeX formatted string for the nested list.
 
     Example:
-        >>> markdown_to_latex(["* Title of Frame", "  * First item", "    * Subitem 1", "* Second item"])
-        "\\begin{frame}{Title of Frame}\n\\begin{itemize}\n  \\item Title of Frame\n    \\item First item\n      \\item Subitem 1\n  \\item Second item\n\\end{itemize}\n\\end{frame}\n"
+        >>> markdown_to_latex([
+            "* Title of Frame",
+            "  * First item",
+            "    * Subitem 1",
+            "* Second item"])
+        "\\begin{frame}{Title of Frame}\n
+        \\begin{itemize}\n
+        \\item Title of Frame\n
+        \\item First item\n
+        \\item Subitem 1\n
+        \\item Second item\n
+        \\end{itemize}\n
+        \\end{frame}\n"
     """
+    hdbg.dassert_isinstance(markdown, str)
+    markdown = markdown.split("\n")
     # Parse markdown into a structured list.
-    items = parse_markdown(markdown)
-
+    items = _parse_markdown(markdown)
     # Determine if the top-level item is a frame.
-    frame_title = items[0].text if items and items[0].text.startswith(
-        "*") else ""
-
+    if items and items[0].text.startswith("*"):
+        frame_title = items[0].text
+    else:
+        frame_title = ""
     # Render LaTeX output from the structured list.
-    return render_latex(items, frame_title=frame_title)
+    txt = render_latex(items, frame_title=frame_title)
+    return txt
 
 
 # #############################################################################
