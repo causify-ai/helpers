7004b49a2011eb6a71a797c0aaf8bee9e54f228c
Sameep2808 <sameepote2808@gmail.com>
2024-11-12 15:06:43 +0000

    CmTask9327_Factor_out_common_code_in_multiple_order_cancellation_methods

--- helpers/test/test_hretry.py
+++ helpers/test/test_hretry.py
@@ -7,7 +7,7 @@ import helpers.hunit_test as hunitest
 
 _LOG = logging.getLogger(__name__)
 
-EXCEPTIONS = (AttributeError, ValueError)
+EXCEPTIONS = {AttributeError: (True, "ATTRIBUTE ERROR"), ValueError: (True, None)}
 
 
 class Test_retry(hunitest.TestCase):
@@ -16,18 +16,18 @@ class Test_retry(hunitest.TestCase):
         Test normal case.
         """
         self.exception_count = 0
-        num_attempts = 3
+        self.num_attempts = 3
 
-        @hretry.sync_retry(num_attempts, EXCEPTIONS)
-        def func() -> bool:
-            if self.exception_count < num_attempts - 1:
+        @hretry.sync_retry("num_attempts", EXCEPTIONS)
+        def func(self) -> bool:
+            if self.exception_count < self.num_attempts - 1:
                 self.exception_count += 1
                 raise ValueError("Simulated expected error")
             _LOG.debug("All good")
             return True
 
-        self.assertTrue(func())
-        self.assertEqual(self.exception_count, num_attempts - 1)
+        self.assertTrue(func(self))
+        self.assertEqual(self.exception_count, self.num_attempts - 1)
 
     def test_retry2(self) -> None:
         """
@@ -35,18 +35,18 @@ class Test_retry(hunitest.TestCase):
         retries.
         """
         self.exception_count = 0
-        num_attempts = 3
+        self.num_attempts = 3
 
-        @hretry.sync_retry(num_attempts, EXCEPTIONS)
-        def func() -> bool:
-            if self.exception_count < num_attempts:
+        @hretry.sync_retry("num_attempts", EXCEPTIONS)
+        def func(self) -> bool:
+            if self.exception_count < self.num_attempts:
                 self.exception_count += 1
                 raise ValueError("Simulated expected error")
             _LOG.debug("All good")
             return True
 
         with self.assertRaises(ValueError):
-            func()
+            func(self)
 
     def test_retry3(self) -> None:
         """
@@ -54,17 +54,17 @@ class Test_retry(hunitest.TestCase):
         exceptions.
         """
         self.exception_count = 0
-        num_attempts = 3
+        self.num_attempts = 3
 
-        @hretry.sync_retry(num_attempts, EXCEPTIONS)
-        def func() -> None:
-            if self.exception_count < num_attempts - 1:
+        @hretry.sync_retry("num_attempts", EXCEPTIONS)
+        def func(self) -> None:
+            if self.exception_count < self.num_attempts - 1:
                 self.exception_count += 1
                 raise IndexError("Simulated non expected error")
             _LOG.debug("All good")
 
         with self.assertRaises(IndexError):
-            func()
+            func(self)
 
 
 class Test_retry2(hunitest.TestCase):
@@ -73,12 +73,14 @@ class Test_retry2(hunitest.TestCase):
         Test normal case.
         """
         self.exception_count = 0
-        num_attempts = 3
+        self.num_attempts = 3
         retry_delay_in_sec = 1
 
-        @hretry.async_retry(num_attempts, EXCEPTIONS, retry_delay_in_sec)
-        async def func() -> bool:
-            if self.exception_count < num_attempts - 1:
+        @hretry.async_retry(
+            "num_attempts", EXCEPTIONS, retry_delay_in_sec=retry_delay_in_sec
+        )
+        async def func(self) -> bool:
+            if self.exception_count < self.num_attempts - 1:
                 self.exception_count += 1
                 await asyncio.sleep(0.1)
                 raise ValueError("Simulated expected error")
@@ -86,10 +88,10 @@ class Test_retry2(hunitest.TestCase):
             return True
 
         with htimer.TimedScope(logging.INFO, "async_retry_loop") as ts:
-            result = asyncio.run(func())
+            result = asyncio.run(func(self))
         self.assertEqual(round(ts.elapsed_time, 1), 2.2)
         self.assertTrue(result)
-        self.assertEqual(self.exception_count, num_attempts - 1)
+        self.assertEqual(self.exception_count, self.num_attempts - 1)
 
     def test_async_retry2(self) -> None:
         """
@@ -97,12 +99,14 @@ class Test_retry2(hunitest.TestCase):
         retries.
         """
         self.exception_count = 0
-        num_attempts = 3
+        self.num_attempts = 3
         retry_delay_in_sec = 1
 
-        @hretry.async_retry(num_attempts, EXCEPTIONS, retry_delay_in_sec)
-        async def func() -> bool:
-            if self.exception_count < num_attempts:
+        @hretry.async_retry(
+            "num_attempts", EXCEPTIONS, retry_delay_in_sec=retry_delay_in_sec
+        )
+        async def func(self) -> bool:
+            if self.exception_count < self.num_attempts:
                 self.exception_count += 1
                 await asyncio.sleep(0.1)
                 raise ValueError("Simulated expected error")
@@ -111,7 +115,7 @@ class Test_retry2(hunitest.TestCase):
 
         with self.assertRaises(ValueError) as fail:
             with htimer.TimedScope(logging.INFO, "async_retry_loop") as ts:
-                asyncio.run(func())
+                asyncio.run(func(self))
         self.assertEqual(round(ts.elapsed_time, 1), 3.3)
         actual = str(fail.exception)
         expected = "Simulated expected error"
@@ -123,19 +127,21 @@ class Test_retry2(hunitest.TestCase):
         exceptions.
         """
         self.exception_count = 0
-        num_attempts = 3
+        self.num_attempts = 3
         retry_delay_in_sec = 1
 
-        @hretry.async_retry(num_attempts, EXCEPTIONS, retry_delay_in_sec)
-        async def func() -> None:
-            if self.exception_count < num_attempts - 1:
+        @hretry.async_retry(
+            "num_attempts", EXCEPTIONS, retry_delay_in_sec=retry_delay_in_sec
+        )
+        async def func(self) -> None:
+            if self.exception_count < self.num_attempts - 1:
                 self.exception_count += 1
                 await asyncio.sleep(0.1)
                 raise IndexError("Simulated non expected error")
             _LOG.debug("All good")
 
         with self.assertRaises(IndexError) as fail:
-            asyncio.run(func())
+            asyncio.run(func(self))
         actual = str(fail.exception)
         expected = "Simulated non expected error"
         self.assert_equal(actual, expected)
