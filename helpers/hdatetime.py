54a0a036794223fba16be7dc7d1064eb1c653b03
tamriq <tamriqua@gmail.com>
2024-11-19 12:24:42 +0000

    CmTask10758: fix timeouterror error

--- helpers/hdatetime.py
+++ helpers/hdatetime.py
@@ -578,8 +578,12 @@ def str_to_timestamp(
     else:
         # Convert using the provided format.
         timestamp = pd.to_datetime(timestamp_as_str, format=datetime_format)
-    # Convert to the specified timezone
-    timestamp = timestamp.tz_localize(tz)
+    if timestamp.tz is None:
+        # Localize the timezone if the timestamp is not timezone-aware.
+        timestamp = timestamp.tz_localize(tz)
+    else:
+        # Convert the timezone if the timestamp is timezone-aware.
+        timestamp = timestamp.tz_convert(tz)
     return timestamp
 
 
