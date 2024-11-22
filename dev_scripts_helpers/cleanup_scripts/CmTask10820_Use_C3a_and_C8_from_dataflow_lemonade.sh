37b7387cf64031727074ba781eddc55d08593040
Nina Lee <nina.tru94@gmail.com>
2024-11-22 17:03:12 +0000

    CmTask10820_Use_C3a_and_C8_from_dataflow_lemonade

---
+++ dev_scripts_helpers/cleanup_scripts/CmTask10820_Use_C3a_and_C8_from_dataflow_lemonade.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -xe
+
+dir_names="amp/helpers_root/dev_scripts_helpers/cleanup_scripts helpers_root/dev_scripts_helpers/cleanup_scripts dev_scripts_helpers/cleanup_scripts"
+
+amp/helpers_root/dev_scripts_helpers/system_tools/replace_text.py \
+    --old "dataflow_orange.pipelines.C3.C3a_pipeline_tmp.C3a_DagBuilder_tmp" \
+    --new "dataflow_lemonade.pipelines.C3.C3a_pipeline_tmp.C3a_DagBuilder" \
+    --exclude_dirs "$dir_names"
+
+amp/helpers_root/dev_scripts_helpers/system_tools/replace_text.py \
+    --old "dataflow_orange.pipelines.C8.C8a_pipeline_tmp.C8a_DagBuilder_tmp" \
+    --new "dataflow_lemonade.pipelines.C8.C8a_pipeline_tmp.C8a_DagBuilder" \
+    --exclude_dirs "$dir_names"
+
+amp/helpers_root/dev_scripts_helpers/system_tools/replace_text.py \
+    --old "dataflow_orange.pipelines.C8.C8b_pipeline_tmp.C8b_DagBuilder_tmp" \
+    --new "dataflow_lemonade.pipelines.C8.C8b_pipeline_tmp.C8b_DagBuilder" \
+    --exclude_dirs "$dir_names"
