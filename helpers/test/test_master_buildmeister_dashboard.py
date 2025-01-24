48f8f4e7eb63fbae35aa63226ecb5a4287f6880e
Heanh Sok <heanhsok@gmail.com>
2025-01-24 21:07:48 +0000

    checkpoint

--- helpers/test/test_master_buildmeister_dashboard.py
+++ helpers/test/test_master_buildmeister_dashboard.py
@@ -55,6 +55,7 @@ class Test_Master_buildmeister_dashboard_notebook(
         not hgit.is_in_amp_as_supermodule(),
         reason="Run only in amp as super-module",
     )
+    @pytest.mark.slow("~25 sec.")
     def test2(self) -> None:
         """
         Check that we can get status for all the workflows.
