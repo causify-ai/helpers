516424bb273798e83b5695f0e5a1e45296d66847
Heanh Sok <heanhsok@gmail.com>
2025-01-24 20:10:48 +0000

    checkpoint

--- helpers/test/test_master_buildmeister_dashboard.py
+++ helpers/test/test_master_buildmeister_dashboard.py
@@ -6,7 +6,7 @@ import pytest
 
 import helpers.hgit as hgit
 import helpers.hserver as hserver
-
+import helpers.lib_tasks_gh as hlitagh
 
 def build_config() -> cconfig.ConfigList:
     """
@@ -37,3 +37,21 @@ class Test_Master_buildmeister_dashboard_notebook(
         )
         config_builder = "helpers.test.test_master_buildmeister_dashboard.build_config()"
         self._test_run_notebook(notebook_path, config_builder)
+
+	@pytest.mark.skipif(
+        not hserver.is_inside_ci(),
+        reason="No access to data from `lemonade` repo locally",
+    )
+    def test2(self) -> None:
+        """
+        Check that we can get status for all the workflows.
+        """
+        repo_list = [
+            "causify-ai/cmamp",
+            "causify-ai/orange",
+            "causify-ai/lemonade",
+            "causify-ai/kaizenflow",
+            "causify-ai/helpers",
+            "causify-ai/quant_dashboard",
+        ]
+        workflow_df = hlitagh.gh_get_details_for_all_workflows(repo_list)
