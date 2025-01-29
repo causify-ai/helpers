aa929c067e1eb3c656c78231212d9cc5df59ecce
Heanh Sok <heanhsok@gmail.com>
2025-01-29 22:01:42 +0000

    checkpoint

--- helpers/lib_tasks_gh.py
+++ helpers/lib_tasks_gh.py
@@ -682,6 +682,28 @@ def gh_get_overall_build_status_for_repo(
     return overall_status
 
 
+def gh_get_workflow_type_names(repo_name: str, *, sort: bool = True) -> List[str]:
+    """
+    Get a list of workflow names for a given repo.
+    :param repo_name: git repo name in the format "organization/repo",
+        e.g., "cryptokaizen/cmamp"
+    :param sort: if True, sort the list of workflow names
+    :return: list of workflow names, e.g., ["Fast tests", "Slow tests"]
+    """
+    hdbg.dassert_isinstance(repo_name, str)
+    _LOG.debug(hprint.to_str("repo_name"))
+    # Get the workflow list.
+    cmd = f"gh workflow list --json name --repo {repo_name}"
+    workflow_types = _gh_run_and_get_json(cmd)
+    workflow_names = [workflow["name"] for workflow in workflow_types]
+    if sort:
+        workflow_names = sorted(workflow_names)
+    # Check for duplicate workflow names.
+    hdbg.dassert_no_duplicates(
+        workflow_names, "Found duplicate workflow names in repo '%s'" % repo_name
+    )
+    return workflow_names
+
 def gh_get_workflows(repo_name: str, *, sort: bool = True) -> List[Dict[str, str]]:
     """
     Get a list of workflow for a given repo.
@@ -700,12 +722,7 @@ def gh_get_workflows(repo_name: str, *, sort: bool = True) -> List[Dict[str, str
     # sort workflow by name
     if sort:
         workflows = sorted(workflows, key=lambda workflow: workflow["name"])
-    # Check for duplicate workflow names.
-    hdbg.dassert_no_duplicates(
-        workflow_names, "Found duplicate workflow names in repo '%s'" % repo_name
-    )
     return workflows
-
 
 def gh_get_workflow_details(
     repo_name: str, workflow_id: str, fields: List[str], limit: int
