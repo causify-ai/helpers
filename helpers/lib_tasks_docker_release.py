e979de46f6d339072447d5cc1bc0c7e04b9d923d
vlady <v.yakovenko@crypto-kaizen.com>
2025-02-27 15:15:45 +0000

    CmampTask11418: fix env

--- helpers/lib_tasks_docker_release.py
+++ helpers/lib_tasks_docker_release.py
@@ -14,13 +14,13 @@ from invoke import task
 # We want to minimize the dependencies from non-standard Python packages since
 # this code needs to run with minimal dependencies and without Docker.
 import helpers.hdbg as hdbg
+import helpers.henv as henv
 import helpers.hgit as hgit
 import helpers.hs3 as hs3
 import helpers.hsystem as hsystem
 import helpers.lib_tasks_docker as hlitadoc
 import helpers.lib_tasks_pytest as hlitapyt
 import helpers.lib_tasks_utils as hlitauti
-import repo_config as rconf
 
 _DEFAULT_TARGET_REGISTRY = "aws_ecr.ck"
 _LOG = logging.getLogger(__name__)
@@ -436,7 +436,9 @@ def docker_tag_push_multi_build_local_image_as_dev(  # type: ignore
         dev_base_image = ""
     elif target_registry == "dockerhub.causify":
         # Use public GitHub Docker registry.
-        base_image_name = rconf.get_docker_base_image_name()
+        base_image_name =  henv.execute_repo_config_code(
+            "get_docker_base_image_name()"
+        ) 
         dev_base_image = f"causify/{base_image_name}"
     else:
         raise ValueError(
