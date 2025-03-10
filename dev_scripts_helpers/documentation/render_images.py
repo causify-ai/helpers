211d7b53f18e81a9b57b312bd07a049c76a7e119
GP Saggese <saggese@gmail.com>
2025-03-10 08:29:50 -0400

    Improve

--- dev_scripts_helpers/documentation/render_images.py
+++ dev_scripts_helpers/documentation/render_images.py
@@ -40,24 +40,6 @@ _LOG = logging.getLogger(__name__)
 # #############################################################################
 
 
-def _open_html(out_file: str) -> None:
-    """
-    Convert the output file to HTML using `notes_to_pdf.py` and open it.
-
-    :param out_file: path to the output file to open
-    """
-    _LOG.info("\n%s", hprint.frame("Convert file to HTML using notes_to_pdf.py"))
-    # Compose the command.
-    cur_path = os.path.abspath(os.path.dirname(__file__))
-    tmp_dir = os.path.split(out_file)[0]
-    cmd = (
-        f"{cur_path}/notes_to_pdf.py -t html -i {out_file} --skip_action "
-        f"copy_to_gdrive --skip_action cleanup_after --tmp_dir {tmp_dir}"
-    )
-    # Run.
-    hsystem.system(cmd)
-
-
 def _get_rendered_file_paths(
     out_file: str, image_code_idx: int, dst_ext: str
 ) -> Tuple[str, str, str]:
@@ -101,12 +83,12 @@ def _get_puppeteer_config_path() -> str:
     """
     Get the path of the puppeteer config file.
 
-    Aborts the script if the file is not found.
-
     :return: path to the config file
     """
     cmd = "find . -name 'puppeteerConfig.json'"
     _, paths_out = hsystem.system_to_string(cmd)
+    # TODO(gp): We expect only one file.
+    hdbg.dassert_eq(len(paths_out.split("\n")), 1)
     # Pick the one closer to the current dir.
     path = sorted(paths_out.split("\n"))[0]
     hdbg.dassert_path_exists(path)
@@ -124,7 +106,7 @@ def _get_render_command(
     Create the command for rendering the image.
 
     :param code_file_path: path to the file with the image code
-    :param abs_img_dir_path: absolute path to a dir where the image will
+    :param abs_img_dir_path: absolute path to the dir where the image will
         be saved
     :param rel_img_path: relative path to the image to be rendered
     :param dst_ext: extension of the rendered image, e.g., "svg", "png"
@@ -137,7 +119,7 @@ def _get_render_command(
     hdbg.dassert_in(dst_ext, valid_extensions)
     # Create the command.
     if image_code_type == "plantuml":
-        cmd = f"plantuml -t{dst_ext} -o {abs_img_dir_path} {code_file_path}"
+        cmd = f"plantuml -t {dst_ext} -o {abs_img_dir_path} {code_file_path}"
     elif image_code_type == "mermaid":
         puppeteer_config = _get_puppeteer_config_path()
         cmd = f"mmdc --puppeteerConfigFile {puppeteer_config} -i {code_file_path} -o {rel_img_path}"
@@ -321,6 +303,27 @@ def _render_images(
 
 # #############################################################################
 
+
+def _open_html(out_file: str) -> None:
+    """
+    Convert the output file to HTML using `notes_to_pdf.py` and open it.
+
+    :param out_file: path to the output file to open
+    """
+    _LOG.info("\n%s", hprint.frame("Convert file to HTML"))
+    # Compose the command.
+    cur_path = os.path.abspath(os.path.dirname(__file__))
+    tmp_dir = os.path.split(out_file)[0]
+    cmd = (
+        f"{cur_path}/notes_to_pdf.py -t html -i {out_file} --skip_action "
+        f"copy_to_gdrive --skip_action cleanup_after --tmp_dir {tmp_dir}"
+    )
+    # Run.
+    hsystem.system(cmd)
+
+
+# #############################################################################
+
 _ACTION_OPEN = "open"
 _ACTION_RENDER = "render"
 _VALID_ACTIONS = [_ACTION_OPEN, _ACTION_RENDER]
@@ -384,7 +387,8 @@ def _main(parser: argparse.ArgumentParser) -> None:
     if actions == [_ACTION_OPEN]:
         # Set the output file path and image extension used for the preview
         # action.
-        out_file = tempfile.mktemp(suffix="." + in_file.split(".")[-1])
+        in_file_ext = os.path.splitext(in_file)[1]
+        out_file = tempfile.mktemp(suffix="." + in_file_ext)
         dst_ext = "svg"
     # Read the input file.
     in_lines = hio.from_file(in_file).split("\n")
--- dev_scripts_helpers/documentation/test/test_render_images.py
+++ dev_scripts_helpers/documentation/test/test_render_images.py
@@ -5,12 +5,10 @@ from typing import List
 
 import dev_scripts_helpers.documentation.render_images as dshdreim
 import helpers.hio as hio
+import helpers.hprint as hprint
 import helpers.hunit_test as hunitest
 
 _LOG = logging.getLogger(__name__)
-
-
-# #############################################################################
 
 
 # #############################################################################
@@ -24,13 +22,21 @@ class Test_get_rendered_file_paths(hunitest.TestCase):
         """
         Check generation of file paths for rendering images.
         """
+        # Prepare inputs.
         out_file = "/a/b/c/d/e.md"
         image_code_idx = 8
         dst_ext = "png"
+        # Run function.
         paths = dshdreim._get_rendered_file_paths(
             out_file, image_code_idx, dst_ext
         )
-        self.check_string("\n".join(paths))
+        # Check output.
+        act = "\n".join(paths)
+        exp = """
+        e.8.txt
+        /a/b/c/d/figs
+        figs/e.8.png"""
+        self.assert_equal(act, exp)
 
 
 # #############################################################################
@@ -40,15 +46,17 @@ class Test_get_rendered_file_paths(hunitest.TestCase):
 
 class Test_get_render_command(hunitest.TestCase):
 
-    def test_get_render_command1(self) -> None:
+    def test1(self) -> None:
         """
         Check that the plantUML command is constructed correctly.
         """
+        # Prepare inputs.
         code_file_path = "/a/b/c.txt"
         abs_img_dir_path = "/d/e/f"
         rel_img_path = "figs/e.8.png"
         dst_ext = "png"
         image_code_type = "plantuml"
+        # Run function.
         cmd = dshdreim._get_render_command(
             code_file_path,
             abs_img_dir_path,
@@ -56,17 +64,21 @@ class Test_get_render_command(hunitest.TestCase):
             dst_ext,
             image_code_type,
         )
-        self.check_string(cmd)
+        # Check output.
+        exp = ""
+        self.assert_equal(cmd, exp)
 
-    def test_get_render_command2(self) -> None:
+    def test2(self) -> None:
         """
         Check that the error is raised if the image extension is unsupported.
         """
+        # Prepare inputs.
         code_file_path = "/a/b/c.txt"
         abs_img_dir_path = "/d/e/f"
         rel_img_path = "figs/e.8.png"
         dst_ext = "bmp"
         image_code_type = "plantuml"
+        # Run function.
         with self.assertRaises(AssertionError) as cm:
             dshdreim._get_render_command(
                 code_file_path,
@@ -78,15 +90,17 @@ class Test_get_render_command(hunitest.TestCase):
         # Check error text.
         self.assertIn("bmp", str(cm.exception))
 
-    def test_get_render_command3(self) -> None:
+    def test3(self) -> None:
         """
         Check that the mermaid command is constructed correctly.
         """
+        # Prepare inputs.
         code_file_path = "/a/b/c.txt"
         abs_img_dir_path = "/d/e/f"
         rel_img_path = "figs/e.8.png"
         image_code_type = "mermaid"
         dst_ext = "png"
+        # Run function.
         cmd = dshdreim._get_render_command(
             code_file_path,
             abs_img_dir_path,
@@ -94,191 +108,222 @@ class Test_get_render_command(hunitest.TestCase):
             dst_ext,
             image_code_type,
         )
+        # Check output.
         # Remove the path to the config file to stabilize the test across repos.
         cmd = re.sub(
             r"--puppeteerConfigFile [\w\.\/]+", r"--puppeteerConfigFile <>", cmd
         )
-        self.check_string(cmd)
+        exp = ""
+        self.assert_equal(cmd, exp)
 
 
 # #############################################################################
-# Test_render_images
+# Test_render_images1
 # #############################################################################
 
 
-class Test_render_images(hunitest.TestCase):
+class Test_render_images1(hunitest.TestCase):
     """
     Test _render_images() with dry run enabled (updating file text without
     creating images).
     """
 
-    def test_render_images1(self) -> None:
+    def test1(self) -> None:
         """
         Check bare plantUML code in a Markdown file.
         """
-        in_lines = [
-            "```plantuml",
-            "Alice --> Bob",
-            "```",
-        ]
+        in_lines = """
+        ```plantuml
+        Alice --> Bob
+        ```
+        """
         file_ext = "md"
         self._update_text_and_check(in_lines, file_ext)
 
-    def test_render_images2(self) -> None:
+    def test2(self) -> None:
         """
         Check plantUML code within other text in a Markdown file.
         """
-        in_lines = [
-            "A",
-            "```plantuml",
-            "Alice --> Bob",
-            "```",
-            "B",
-        ]
+        in_lines = """
+        A
+        ```plantuml
+        Alice --> Bob
+        ```
+        B
+        """
         file_ext = "md"
         self._update_text_and_check(in_lines, file_ext)
 
-    def test_render_images3(self) -> None:
+    def test3(self) -> None:
         """
         Check text without image code in a Markdown file.
         """
-        in_lines = [
-            "A",
-            "```bash",
-            "Alice --> Bob",
-            "```",
-            "B",
-        ]
+        in_lines = """
+        A
+        ```bash
+        Alice --> Bob
+        ```
+        B
+        """
         file_ext = "md"
         self._update_text_and_check(in_lines, file_ext)
 
-    def test_render_images4(self) -> None:
+    def test4(self) -> None:
         """
         Check plantUML code that is already correctly formatted in a Markdown
         file.
         """
-        in_lines = [
-            "```plantuml",
-            "@startuml",
-            "Alice --> Bob",
-            "@enduml",
-            "```",
-        ]
+        in_lines = """
+        ```plantuml
+        @startuml
+        Alice --> Bob
+        @enduml
+        ```
+        """
         file_ext = "md"
         self._update_text_and_check(in_lines, file_ext)
 
-    def test_render_images5(self) -> None:
+    def test5(self) -> None:
         """
         Check bare mermaid code in a Markdown file.
         """
-        in_lines = [
-            "```mermaid",
-            "flowchart TD;",
-            "  A[Start] --> B[End];",
-            "```",
-        ]
+        in_lines = """
+        ```mermaid
+        flowchart TD;
+          A[Start] --> B[End];
+        ```
+        """
         file_ext = "md"
         self._update_text_and_check(in_lines, file_ext)
 
-    def test_render_images6(self) -> None:
+    def test6(self) -> None:
         """
         Check mermaid code within other text in a Markdown file.
         """
-        in_lines = [
-            "A",
-            "```mermaid",
-            "flowchart TD;",
-            "  A[Start] --> B[End];",
-            "```",
-            "B",
-        ]
+        in_lines = """
+        A
+        ```mermaid
+        flowchart TD;
+          A[Start] --> B[End];
+        ```
+        B
+        """
         file_ext = "md"
         self._update_text_and_check(in_lines, file_ext)
 
-    def test_render_images7(self) -> None:
+    def test7(self) -> None:
         """
         Check bare plantUML code in a LaTeX file.
         """
-        in_lines = [
-            "```plantuml",
-            "Alice --> Bob",
-            "```",
-        ]
+        in_lines = """
+        ```plantuml
+        Alice --> Bob
+        ```
+        """
         file_ext = "tex"
         self._update_text_and_check(in_lines, file_ext)
 
-    def test_render_images8(self) -> None:
+    def test8(self) -> None:
         """
         Check plantUML code within other text in a LaTeX file.
         """
-        in_lines = [
-            "A",
-            "```plantuml",
-            "Alice --> Bob",
-            "```",
-            "B",
-        ]
+        in_lines = """
+        A
+        ```plantuml
+        Alice --> Bob
+        ```
+        B
+        """
         file_ext = "tex"
         self._update_text_and_check(in_lines, file_ext)
 
-    def test_render_images9(self) -> None:
+    def test9(self) -> None:
         """
         Check text without image code in a LaTeX file.
         """
-        in_lines = [
-            "A",
-            "```bash",
-            "Alice --> Bob",
-            "```",
-            "B",
-        ]
+        in_lines = """
+        A
+        ```bash
+        Alice --> Bob
+        ```
+        B
+        """
         file_ext = "tex"
         self._update_text_and_check(in_lines, file_ext)
 
-    def test_render_images10(self) -> None:
+    def test10(self) -> None:
         """
         Check plantUML code that is already correctly formatted in a LaTeX
         file.
         """
-        in_lines = [
-            "```plantuml",
-            "@startuml",
-            "Alice --> Bob",
-            "@enduml",
-            "```",
-        ]
+        in_lines = """
+        ```plantuml
+        @startuml
+        Alice --> Bob
+        @enduml
+        ```
+        """
         file_ext = "tex"
         self._update_text_and_check(in_lines, file_ext)
 
-    def test_render_images11(self) -> None:
+    def test11(self) -> None:
         """
         Check bare mermaid code in a LaTeX file.
         """
-        in_lines = [
-            "```mermaid",
-            "flowchart TD;",
-            "  A[Start] --> B[End];",
-            "```",
-        ]
+        in_lines = """
+        ```mermaid
+        flowchart TD;
+          A[Start] --> B[End];
+        ```
+        """
         file_ext = "tex"
         self._update_text_and_check(in_lines, file_ext)
 
-    def test_render_images12(self) -> None:
+    def test12(self) -> None:
         """
         Check mermaid code within other text in a LaTeX file.
         """
-        in_lines = [
-            "A",
-            "```mermaid",
-            "flowchart TD;",
-            "  A[Start] --> B[End];",
-            "```",
-            "B",
-        ]
+        in_lines = """
+        A
+        ```mermaid
+        flowchart TD;
+          A[Start] --> B[End];
+        ```
+        B
+        """
         file_ext = "tex"
         self._update_text_and_check(in_lines, file_ext)
 
-    def test_render_images_playback1(self) -> None:
+    # ///////////////////////////////////////////////////////////////////////////
+
+    def _update_text_and_check(self, txt: str, file_ext: str) -> None:
+        """
+        Check that the text is updated correctly.
+
+        :param txt: the input text
+        :param file_ext: the extension of the input file
+        """
+        # Prepare inputs.
+        txt = hprint.dedent(txt, remove_lead_trail_empty_lines_=True).split("\n")
+        out_file = os.path.join(self.get_scratch_space(), f"out.{file_ext}")
+        dst_ext = "png"
+        # Render images.
+        out_lines = dshdreim._render_images(
+            txt, out_file, dst_ext, run_dockerized=True, dry_run=True
+        )
+        # Check output.
+        act = "\n".join(out_lines)
+        self.check_string(act)
+
+
+# #############################################################################
+# Test_render_images2
+# #############################################################################
+
+
+class Test_render_images2(hunitest.TestCase):
+
+    def test1(self) -> None:
         """
         Test running on a real Markdown file with plantUML code.
         """
@@ -302,7 +347,7 @@ class Test_render_images(hunitest.TestCase):
         # Check output.
         self.check_string(act)
 
-    def test_render_images_playback2(self) -> None:
+    def test2(self) -> None:
         """
         Test running on a real Markdown file with mermaid code.
         """
@@ -326,7 +371,7 @@ class Test_render_images(hunitest.TestCase):
         # Check output.
         self.check_string(act)
 
-    def test_render_images_playback3(self) -> None:
+    def test3(self) -> None:
         """
         Test running on a full LaTeX file with plantUML code.
         """
@@ -350,7 +395,7 @@ class Test_render_images(hunitest.TestCase):
         # Check output.
         self.check_string(act)
 
-    def test_render_images_playback4(self) -> None:
+    def test4(self) -> None:
         """
         Test running on a full LaTeX file with mermaid code.
         """
@@ -373,17 +418,3 @@ class Test_render_images(hunitest.TestCase):
         act = "\n".join(out_lines)
         # Check output.
         self.check_string(act)
-
-    def _update_text_and_check(self, in_lines: List[str], file_ext: str) -> None:
-        """
-        Check that the text is updated correctly.
-
-        :param in_lines: the lines of the input file
-        :param file_ext: the extension of the input file
-        """
-        out_file = os.path.join(self.get_scratch_space(), f"out.{file_ext}")
-        dst_ext = "png"
-        out_lines = dshdreim._render_images(
-            in_lines, out_file, dst_ext, run_dockerized=True, dry_run=True
-        )
-        self.check_string("\n".join(out_lines))
