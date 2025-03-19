22789d86bca90958531658f8e8e6509e81e7e964
vlady <v.yakovenko@crypto-kaizen.com>
2025-03-19 17:18:03 +0000

    CmampTask11193: revert ci condition

--- helpers/lib_tasks_docker.py
+++ helpers/lib_tasks_docker.py
@@ -1308,11 +1308,7 @@ def _get_lint_docker_cmd(
     :param stage: the image stage to use
     :return: the full command to run
     """
-    # Infer the docker registry based on the environment.
-    if hserver.is_dev_ck() or hserver.is_inside_ci():
-        base_path = os.environ["CSFY_ECR_BASE_PATH"]
-    else:
-        base_path = "causify"
+    base_path = os.environ["CSFY_ECR_BASE_PATH"]
     _LOG.debug("base_path=%s", base_path)
     # Get an image to run the linter on.
     linter_image = f"{base_path}/helpers"
