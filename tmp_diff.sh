018a78da699a24752f9a2e52f5f8ebea80805224
Indrayudd Roy Chowdhury <indro@dhcp-10-175-250-54.umd.edu>
2025-03-24 15:31:22 -0400

    Remove unwanted files from remote

--- tmp_diff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ $1 == "wrap" ]]; then
-    cmd='vimdiff -c "windo set wrap"'
-else
-    cmd='vimdiff'
-fi;
-cmd="$cmd helpers/test/outcomes/TestCheckDataFrame1.test_check_df_not_equal4/tmp.final.actual.txt helpers/test/outcomes/TestCheckDataFrame1.test_check_df_not_equal4/tmp.final.expected.txt"
-eval $cmd
