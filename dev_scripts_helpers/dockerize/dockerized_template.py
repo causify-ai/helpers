9ecac81e3aff5c10210494abbaa12b1dbc48a5c4
shaunak01 <shaunakdhande4000@gmail.com>
2025-03-27 02:26:17 +0000

    checkpoint

--- dev_scripts_helpers/dockerize/dockerized_template.py
+++ dev_scripts_helpers/dockerize/dockerized_template.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """
 Convert Docx file to Markdown.
 
@@ -16,57 +15,8 @@ import logging
 import helpers.hdbg as hdbg
 import helpers.hdocker as hdocker
 import helpers.hparser as hparser
-import helpers.hprint as hprint
-import helpers.hsystem as hsystem
 
 _LOG = logging.getLogger(__name__)
-
-
-def _build_container() -> str:
-    container_name = "convert_docx_to_markdown"
-    txt = r"""
-    FROM ubuntu:latest
-
-    RUN apt-get update && \
-        apt-get -y upgrade
-
-    RUN apt-get install -y curl pandoc && \
-        apt-get clean && \
-        rm -rf /var/lib/apt/lists/*
-    """
-    txt = hprint.dedent()
-    force_rebuild = False
-    use_sudo = False
-    container_name = hdocker.build_container_image(container_name, txt,
-                                                   force_rebuild, use_sudo)
-    return container_name
-
-
-def _convert_docx_to_markdown(
-    docx_file: str, md_file: str, md_file_figs: str
-) -> None:
-    """
-    Convert Docx file to Markdown.
-
-    :param docx_file: path to the Docx file
-    :param md_file: path to the Markdown file
-    :param md_file_figs: the folder containing the figures for Markdown
-        file
-    """
-    _LOG.info("Converting Docx to Markdown...")
-    hdbg.dassert_file_exists(docx_file)
-    # Create the Markdown file.
-    hsystem.system(f"touch {md_file}")
-    docker_container_name = _build_container()
-    # Run Docker container.
-    cmd = f"rm -rf {md_file_figs}"
-    hsystem.system(cmd)
-    # Convert from Docx to Markdown.
-    cmd = f"pandoc --extract-media {md_file_figs} -f docx -t markdown_strict -{md_file} {docx_file}"
-    hdocker.run_container(docker_container_name, cmd)
-
-
-# #############################################################################
 
 
 def _parse() -> argparse.ArgumentParser:
@@ -75,17 +25,15 @@ def _parse() -> argparse.ArgumentParser:
     )
     parser.add_argument(
         "--docx_file",
-        action="store",
         required=True,
         type=str,
-        help="The Docx file that needs to be converted to Markdown",
+        help="Path to the Docx file to convert.",
     )
     parser.add_argument(
         "--md_file",
-        action="store",
         required=True,
         type=str,
-        help="The output Markdown file",
+        help="Path to the output Markdown file.",
     )
     hparser.add_dockerized_script_arg(parser)
     hparser.add_verbosity_arg(parser)
@@ -94,13 +42,21 @@ def _parse() -> argparse.ArgumentParser:
 
 def _main(parser: argparse.ArgumentParser) -> None:
     args = parser.parse_args()
-    hdbg.init_logger(verbosity=args.log_level, use_exec_path=True)
-    docx_file = args.docx_file
-    md_file = args.md_file
-    # The folder for the figures.
-    md_file_figs = md_file.replace(".md", "_figs")
-    _convert_docx_to_markdown(docx_file, md_file, md_file_figs)
-    _LOG.info("Finished converting '%s' to '%s'.", docx_file, md_file)
+    hdbg.init_logger(
+        verbosity=args.log_level, use_exec_path=True, force_white=False
+    )
+    # Define the folder for extracted figures.
+    md_file_figs = args.md_file.replace(".md", "_figs")
+    _LOG.info("Converting '%s' to Markdown...", args.docx_file)
+    # Run the dockerized conversion function.
+    hdocker.run_dockerized_pandoc(
+        args.docx_file,
+        args.md_file,
+        md_file_figs,
+        force_rebuild=args.dockerized_force_rebuild,
+        use_sudo=args.dockerized_use_sudo,
+    )
+    _LOG.info("Finished converting '%s' to '%s'.", args.docx_file, args.md_file)
 
 
 if __name__ == "__main__":
