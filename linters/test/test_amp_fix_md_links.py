22cb2fd0ab47a3feee52d28c703b1827d7e55402
GP Saggese <saggese@gmail.com>
2025-04-06 11:43:10 -0400

    Improve

--- linters/test/test_amp_fix_md_links.py
+++ linters/test/test_amp_fix_md_links.py
@@ -15,157 +15,45 @@ _LOG = logging.getLogger(__name__)
 # #############################################################################
 
 def _get_output_string(out_warnings: List[str], updated_lines: List[str]) -> str:
-      warnings = "\n".join(out_warnings)
-      lines = "\n".join(updated_lines)
-      output = f"""# linter warnings
-      {warnings}
+    """
+    Get the canonical output string for a test.
+    """
+    warnings = "\n".join(out_warnings)
+    lines = "\n".join(updated_lines)
+    output = f"""
+# linter warnings
+{warnings}
 
-      # linted file
-      {lines}"""
-      return output
+# linted file
+{lines}"""
+    output = hprint.dedent(output)
+    return output
 
 
 class Test_fix_links(hunitest.TestCase):
+
+    def write_input_file(self, txt: str, file_name: str) -> str:
+        """
+        Write test content to a file in the scratch space.
+
+        :param txt: the content of the file
+        :param file_name: the name of the file
+        :return: the path to the file with the test content
+        """
+        txt = hprint.dedent(txt)
+        # Get file path to write.
+        dir_name = self.get_scratch_space()
+        file_path = os.path.join(dir_name, file_name)
+        file_path = os.path.abspath(file_path)
+        # Create the file.
+        hio.to_file(file_path, txt)
+        return file_path
 
     def test1(self) -> None:
         """
         Test fixing link formatting in a Markdown file.
         """
-        # Create a Markdown file with a variety of links.
-        txt_incorrect = self._get_txt_with_incorrect_links()
-        file_name = "test.md"
-        file_path = self._write_input_file(txt_incorrect, file_name)
-        # Run.
-        _, updated_lines, out_warnings = lafimdli.fix_links(file_path)
-        # Check.
-        output = _get_output_string(out_warnings, updated_lines)
-        self.check_string(output)
-
-    def test2(self) -> None:
-        """
-        Test dealing with internal links in a Markdown file.
-        """
-        # Create a Markdown file with internal links, including TOC.
-        txt_internal_links = r"""
-        <!--ts-->
-          * [Best practices for writing plotting functions](#best-practices-for-writing-plotting-functions)
-              * [Cosmetic requirements](#cosmetic-requirements)
-
-        <!--te-->
-
-        <!-- toc -->
-
-        - [Best practices for writing plotting functions](#best-practices-for-writing-plotting-functions)
-          * [Cosmetic requirements](#cosmetic-requirements)
-
-        <!-- tocstop -->
-
-
-        [Data Availability](#data-availability)
-        """
-        txt_internal_links = hprint.dedent(txt_internal_links)
-        #
-        file_name = "test.md"
-        file_path = self._write_input_file(txt_internal_links, file_name)
-        # Run.
-        _, updated_lines, out_warnings = lafimdli.fix_links(file_path)
-        # Check.
-        output = _get_output_string(out_warnings, updated_lines)
-        self.check_string(output)
-
-    def test3(self) -> None:
-        """
-        Test the mix of Markdown and HTML-style links.
-        """
-        input_content = r"""
-        Markdown link: [Valid Markdown Link](docs/markdown_example.md)
-
-        HTML-style link: <a href="docs/html_example.md">Valid HTML Link</a>
-
-        Broken Markdown link: [Broken Markdown Link](missing_markdown.md)
-
-        Broken HTML link: <a href="missing_html.md">Broken HTML Link</a>
-
-        External Markdown link: [External Markdown Link](https://example.com)
-
-        External HTML link: <a href="https://example.com">External HTML Link</a>
-
-        Nested HTML link with Markdown: <a href="[Example](nested.md)">Invalid Nested</a>
-        """
-        file_name = "test_combined.md"
-        file_path = self._write_input_file(input_content, file_name)
-        # Run.
-        _, updated_lines, out_warnings = lafimdli.fix_links(file_path)
-        # Check.
-        output = _get_output_string(out_warnings, updated_lines)
-        self.check_string(output)
-
-    def test4(self) -> None:
-        """
-        Test links with a filepath with a tag ("/image.png") to check for its
-        preservation.
-        """
         # Prepare inputs.
-        input_content = """
-
-        <img src="figs/ck.github_projects_process.reference_figs/image1.png"
-            style="width:6.5in;height:0.31944in" />
-
-        """
-        file_name = "test_excerpt.md"
-        file_path = self._write_input_file(input_content, file_name)
-        # Run.
-        _, updated_lines, _ = lafimdli.fix_links(file_path)
-        # Check.
-        actual = "\n".join(updated_lines)
-        expected = r"""
-
-        <img src="figs/ck.github_projects_process.reference_figs/image1.png"
-            style="width:6.5in;height:0.31944in" />
-
-        """
-        self.assert_equal(actual, expected, fuzzy_match=True)
-
-    def test5(self) -> None:
-        """
-        Test Markdown file references to another Markdown file and its headers.
-        """
-        reference_file_md_content = """
-        # Reference test file
-
-        - [Introduction](#introduction)
-        - [Hyphen test](#hyphen-test)
-
-        ## Introduction
-
-        A test header with one word in the reference file.
-
-        ## Hyphen test
-
-        A test to check two words header in the reference file.
-        """
-        reference_file_name = "reference.md"
-        reference_file_link = self._write_input_file(
-            reference_file_md_content, reference_file_name
-        )
-        test_md_content = rf"""
-        Markdown link: [Valid Markdown and header Link]({reference_file_link}#introduction)
-
-        Markdown link: [InValid Markdown Link](docs/markdown_exam.md#introduction)
-
-        Markdown link: [Invalid header in the Markdown Link]({reference_file_link}#introduce)
-
-        Markdown link: [Valid Markdown and header Link]({reference_file_link}#hyphen-test)
-        """
-        test_file_name = "valid_header_test.md"
-        test_file_link = self._write_input_file(test_md_content, test_file_name)
-        # Run.
-        _, updated_lines, out_warnings = lafimdli.fix_links(test_file_link)
-        # Check.
-        output = _get_output_string(out_warnings, updated_lines)
-        self.check_string(output, purify_text=True)
-
-    def _get_txt_with_incorrect_links(self) -> str:
         txt_incorrect = r"""
         - Markdown-style link with a text label
           - [Here](/helpers/hdbg.py)
@@ -270,26 +158,139 @@ class Test_fix_links(hunitest.TestCase):
         - Markdown-style figure pointer that does not exist
           - ![](/iiimport_check/example/output/basicccc.png)
         """
-        return txt_incorrect
+        file_name = "test.md"
+        file_path = self.write_input_file(txt_incorrect, file_name)
+        # Run.
+        _, updated_lines, out_warnings = lafimdli.fix_links(file_path)
+        # Check.
+        output = _get_output_string(out_warnings, updated_lines)
+        self.check_string(output, purify_text=True)
 
-    def _write_input_file(self, txt: str, file_name: str) -> str:
+    def test2(self) -> None:
         """
-        Write test content to the file.
-
-        :param txt: the content of the file
-        :param file_name: the name of the file
-        :return: the path to the file with the test content
+        Test dealing with internal links in a Markdown file, including TOC.
         """
-        txt = hprint.dedent(txt)
-        # Get the path to the scratch space.
-        dir_name = self.get_scratch_space()
-        # Compile the path to the test content file.
-        file_path = os.path.join(dir_name, file_name)
-        file_path = os.path.abspath(file_path)
-        # Create the file.
-        hio.to_file(file_path, txt)
-        return file_path
+        # Prepare inputs.
+        txt_internal_links = r"""
+        <!--ts-->
+          * [Best practices for writing plotting functions](#best-practices-for-writing-plotting-functions)
+              * [Cosmetic requirements](#cosmetic-requirements)
 
+        <!--te-->
+
+        <!-- toc -->
+
+        - [Best practices for writing plotting functions](#best-practices-for-writing-plotting-functions)
+          * [Cosmetic requirements](#cosmetic-requirements)
+
+        <!-- tocstop -->
+
+
+        [Data Availability](#data-availability)
+        """
+        #
+        file_name = "test.md"
+        file_path = self.write_input_file(txt_internal_links, file_name)
+        # Run.
+        _, updated_lines, out_warnings = lafimdli.fix_links(file_path)
+        # Check.
+        output = _get_output_string(out_warnings, updated_lines)
+        self.check_string(output, purify_text=True)
+
+    def test3(self) -> None:
+        """
+        Test the mix of Markdown and HTML-style links.
+        """
+        # Prepare inputs.
+        input_content = r"""
+        Markdown link: [Valid Markdown Link](docs/markdown_example.md)
+
+        HTML-style link: <a href="docs/html_example.md">Valid HTML Link</a>
+
+        Broken Markdown link: [Broken Markdown Link](missing_markdown.md)
+
+        Broken HTML link: <a href="missing_html.md">Broken HTML Link</a>
+
+        External Markdown link: [External Markdown Link](https://example.com)
+
+        External HTML link: <a href="https://example.com">External HTML Link</a>
+
+        Nested HTML link with Markdown: <a href="[Example](nested.md)">Invalid Nested</a>
+        """
+        file_name = "test_combined.md"
+        file_path = self.write_input_file(input_content, file_name)
+        # Run.
+        _, updated_lines, out_warnings = lafimdli.fix_links(file_path)
+        # Check.
+        output = _get_output_string(out_warnings, updated_lines)
+        self.check_string(output, purify_text=True)
+
+    def test4(self) -> None:
+        """
+        Test links with a filepath with a tag ("/image.png") to check for its
+        preservation.
+        """
+        # Prepare inputs.
+        input_content = r"""
+
+        <img src="figs/ck.github_projects_process.reference_figs/image1.png"
+            style="width:6.5in;height:0.31944in" />
+
+        """
+        file_name = "test_excerpt.md"
+        file_path = self.write_input_file(input_content, file_name)
+        # Run.
+        _, updated_lines, _ = lafimdli.fix_links(file_path)
+        # Check.
+        actual = "\n".join(updated_lines)
+        expected = r"""
+
+        <img src="figs/ck.github_projects_process.reference_figs/image1.png"
+            style="width:6.5in;height:0.31944in" />
+
+        """
+        self.assert_equal(actual, expected, fuzzy_match=True, purify_text=True)
+
+    def test5(self) -> None:
+        """
+        Test Markdown file references to another Markdown file and its headers.
+        """
+        # Prepare inputs.
+        reference_file_md_content = r"""
+        # Reference test file
+
+        - [Introduction](#introduction)
+        - [Hyphen test](#hyphen-test)
+
+        ## Introduction
+
+        A test header with one word in the reference file.
+
+        ## Hyphen test
+
+        A test to check two words header in the reference file.
+        """
+        reference_file_name = "reference.md"
+        reference_file_link = self.write_input_file(
+            reference_file_md_content, reference_file_name
+        )
+        #
+        test_md_content = rf"""
+        Markdown link: [Valid Markdown and header Link]({reference_file_link}#introduction)
+
+        Markdown link: [InValid Markdown Link](docs/markdown_exam.md#introduction)
+
+        Markdown link: [Invalid header in the Markdown Link]({reference_file_link}#introduce)
+
+        Markdown link: [Valid Markdown and header Link]({reference_file_link}#hyphen-test)
+        """
+        test_file_name = "valid_header_test.md"
+        test_file_link = self.write_input_file(test_md_content, test_file_name)
+        # Run.
+        _, updated_lines, out_warnings = lafimdli.fix_links(test_file_link)
+        # Check.
+        output = _get_output_string(out_warnings, updated_lines)
+        self.check_string(output, purify_text=True)
 
 # #############################################################################
 # Test_make_path_absolute
