db6a8fff30a4c1200c26c62f2f91f5d8c52c8826
Shr3yash <12shreyashh@gmail.com>
2025-04-07 11:15:03 +0530

    HelpersTask407_Rename__ck_to__csfy

--- helpers/test/test_hserver.py
+++ helpers/test/test_hserver.py
@@ -23,7 +23,7 @@ class Test_hserver1(hunitest.TestCase):
         # We always run tests inside Docker.
         self.assertTrue(hserver.is_inside_docker())
 
-    def test_is_dev_ck1(self) -> None:
+    def test_is_dev_csfy(self) -> None:
         _ = hserver.is_dev_csfy()
 
     def test_is_prod_csfy1(self) -> None:
