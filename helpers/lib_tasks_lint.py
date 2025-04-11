7bcc351f8584c79d1208ae1033f494ebe5b8aba7
Peeyush4 <peeyu704@gmail.com>
2025-04-11 11:39:27 -0400

    Change in comment for lib_tasks_lint.py

--- helpers/lib_tasks_lint.py
+++ helpers/lib_tasks_lint.py
@@ -221,7 +221,7 @@ def lint(  # type: ignore
     :param only_format: run only the modifying actions of Linter (e.g., black)
     :param only_check: run only the non-modifying actions of Linter (e.g., pylint)
     """
-    # Check if the user is in a git repo.
+    # Check if the user is in a git repo root dir.
     hdbg.dassert(
         hgit.is_cwd_git_repo(),
         msg="Linter should run from git repo root dir",
