935521e7e2a7b1ec48f8c75fc87a033c8b9ab1cf
Allen Mathews <amathews699@gmail.com>
2025-04-11 12:48:30 -0400

    Factored function indices retreival in hstring.py, updated test cases and other nits

--- linters/amp_remove_empty_lines.py
+++ linters/amp_remove_empty_lines.py
@@ -8,58 +8,17 @@ import linters.amp_remove_empty_lines as lareemli
 """
 
 import argparse
-import re
 from typing import List
 
 import helpers.hdbg as hdbg
 import helpers.hio as hio
 import helpers.hparser as hparser
+import helpers.hstring as hstring
 import linters.action as liaction
 import linters.utils as liutils
 
 
-def _clean_function_block(
-    func_lines: List[str], docstring_done: bool, docstring_open: bool
-) -> List[str]:
-    """
-    Remove empty lines inside function.
-
-    :param func_lines: sequence of function lines
-    :param docstring_done: indicates whether docstring is processed
-        completely
-    :param docstring_open: indicates whether docstring is currently
-        processed
-    :return: function without empty lines
-    """
-    cleaned_func_lines = []
-    for line in func_lines:
-        stripped_line = line.strip()
-        if not docstring_done and (
-            stripped_line.startswith('"""') or stripped_line.startswith("'''")
-        ):
-            # Skip if empty line is inside the docstring.
-            docstring_open = not docstring_open
-            cleaned_func_lines.append(line)
-            if (
-                stripped_line.endswith('"""')
-                or stripped_line.endswith("'''")
-                and len(stripped_line) > 3
-            ):
-                docstring_done = True
-            continue
-        if docstring_open:
-            cleaned_func_lines.append(line)
-            if stripped_line.endswith('"""') or stripped_line.endswith("'''"):
-                docstring_open = False
-                docstring_done = True
-            continue
-        if stripped_line == "":
-            continue
-        cleaned_func_lines.append(line)
-    return cleaned_func_lines
-
-
-def update_function_blocks(file_content: str) -> str:
+def update_function_blocks(file_content: str) -> List[str]:
     """
     Process file to find and update functions.
 
@@ -69,50 +28,20 @@ def update_function_blocks(file_content: str) -> str:
     lines = file_content.splitlines()
     if file_content.endswith("\n"):
         lines.append("")
+    docstring_indices = set(hstring.get_docstring_line_indices(lines))
+    function_indices = set(hstring.get_function_indices(lines))
     cleaned_file = []
-    i = 0
-    n = len(lines)
-    # Process each line to find function header.
-    while i < n:
-        line = lines[i]
+    for i, line in enumerate(lines):
         stripped = line.strip()
-        # Check if the function header matches.
-        if stripped.startswith("def ") and (re.match(r"\s*def\s+\w+", line)):
-            func_lines = [line]
-            i += 1
-            indent_match = re.match(r"(\s*)def", line)
-            base_indent = len(indent_match.group(1)) if indent_match else 0
-            # Store all lines of the function separately.
-            while i < n:
-                next_line = lines[i]
-                if next_line.strip() == "":
-                    func_lines.append(next_line)
-                    i += 1
-                    continue
-                # Check indentation.
-                next_indent = len(next_line) - len(next_line.lstrip())
-                if next_indent <= base_indent:
-                    break
-                func_lines.append(next_line)
-                i += 1
-            # Clean the function.
-            docstring_open = False
-            docstring_done = False
-            cleaned_func_block = _clean_function_block(
-                func_lines, docstring_done, docstring_open
-            )
-            for item in reversed(func_lines):
-                # Empty lines after the function is included in the function
-                # so it must be added separately.
-                if item == "":
-                    cleaned_func_block.append("")
-                else:
-                    break
-            cleaned_file.extend(cleaned_func_block)
-        else:
-            cleaned_file.append(line)
-            i += 1
-    return "\n".join(cleaned_file)
+        # Skip empty lines, except in docstrings.
+        if (
+            i in function_indices
+            and i not in docstring_indices
+            and stripped == ""
+        ):
+            continue
+        cleaned_file.append(line)
+    return cleaned_file
 
 
 # #############################################################################
@@ -135,7 +64,7 @@ class _RemoveEmptyLines(liaction.Action):
         updated_lines = update_function_blocks(file_content)
         # Save the updated file with cleaned functions.
         liutils.write_file_back(
-            file_name, file_content.split("\n"), updated_lines.split("\n")
+            file_name, file_content.split("\n"), updated_lines
         )
         return []
 
--- linters/test/test_amp_remove_empty_lines.py
+++ linters/test/test_amp_remove_empty_lines.py
@@ -19,32 +19,13 @@ class Test_remove_empty_lines(hunitest.TestCase):
         test_input_dir = self.get_input_dir()
         text_file_path = os.path.join(test_input_dir, "test.txt")
         text = hio.from_file(text_file_path)
-        expected = '''import os
-import numpy as np
-import pandas as pd
-
-from typing import List, Tuple
-
-def add(a: int, b: int) -> int:
-    """
-    Return the sum of 2 numbers.
-
-    :param a: first number
-    :param b: second number
-    :return: sum of 2 numbers
-    """
-    print("Inside function add()")
-    return a + b
-
-
-result = add(2, 3)
-
-print("Result: ", result)
-print("Outside function")'''
         # Run.
         actual = lareemli.update_function_blocks(text)
         # Check.
-        self.assertEqual(expected, actual)
+        test_output_dir = self.get_output_dir()
+        output_file_path = os.path.join(test_output_dir, "test.txt")
+        expected = hio.from_file(output_file_path)
+        self.assertEqual(expected, "\n".join(actual))
 
     def test2(self) -> None:
         """
@@ -53,50 +34,13 @@ print("Outside function")'''
         test_input_dir = self.get_input_dir()
         text_file_path = os.path.join(test_input_dir, "test.txt")
         text = hio.from_file(text_file_path)
-        expected = '''import os
-import numpy as np
-import pandas as pd
-
-from typing import List, Tuple
-
-
-# #############################################################################
-# Calculator
-# #############################################################################
-
-class Calculator:
-
-    def add(self, a: int, b: int) -> int:
-        """
-        Return the sum of 2 numbers.
-
-        :param a: first number
-        :param b: second number
-        :return: sum of 2 numbers
-        """
-        print("Inside function add()")
-        return a + b
-
-    def subtract(self, a: int, b: int) -> int:
-        """
-        Return the difference of 2 numbers.
-
-        :param a: first number
-        :param b: second number
-        :return: difference of 2 numbers
-        """
-        print("Inside function subtract()")
-        return a - b
-
-
-calc = Calculator()
-
-print(calc.add(10, 5))
-print(calc.subtract(10, 5))'''
         # Run.
         actual = lareemli.update_function_blocks(text)
         # Check.
-        self.assertEqual(expected, actual)
+        test_output_dir = self.get_output_dir()
+        output_file_path = os.path.join(test_output_dir, "test.txt")
+        expected = hio.from_file(output_file_path)
+        self.assertEqual(expected, "\n".join(actual))
 
     def test3(self) -> None:
         """
@@ -105,67 +49,13 @@ print(calc.subtract(10, 5))'''
         test_input_dir = self.get_input_dir()
         text_file_path = os.path.join(test_input_dir, "test.txt")
         text = hio.from_file(text_file_path)
-        expected = '''import os
-import numpy as np
-import pandas as pd
-
-from typing import List, Tuple
-
-def check_largest(a: int, b: int) -> None:
-    """
-    Display largest among two numbers.
-
-    :param a: first number to compare
-    :param b: second number to compare
-    """
-    print("Inside check_largest()")
-    if a > b:
-        print(a, "is largest")
-    elif b > a:
-        print(b, "is largest")
-    else:
-        print("Both are same")
-
-# #############################################################################
-# Calculator
-# #############################################################################
-
-class Calculator:
-
-    def add(self, a: int, b: int) -> int:
-        """
-        Return the sum of 2 numbers.
-
-        :param a: first number
-        :param b: second number
-        :return: sum of 2 numbers
-        """
-        print("Inside function add()")
-        return a + b
-
-    def subtract(self, a: int, b: int) -> int:
-        """
-        Return the difference of 2 numbers.
-
-        :param a: first number
-        :param b: second number
-        :return: difference of 2 numbers
-        """
-        print("Inside function subtract()")
-        return a - b
-
-a = 10
-b = 5
-
-check_largest(a, b)
-
-calc = Calculator()
-print(calc.add(a, b))
-print(calc.subtract(a, b))'''
         # Run.
         actual = lareemli.update_function_blocks(text)
         # Check.
-        self.assertEqual(expected, actual)
+        test_output_dir = self.get_output_dir()
+        output_file_path = os.path.join(test_output_dir, "test.txt")
+        expected = hio.from_file(output_file_path)
+        self.assertEqual(expected, "\n".join(actual))
 
     def test4(self) -> None:
         """
@@ -175,29 +65,10 @@ print(calc.subtract(a, b))'''
         test_input_dir = self.get_input_dir()
         text_file_path = os.path.join(test_input_dir, "test.txt")
         text = hio.from_file(text_file_path)
-        expected = '''import os
-import numpy as np
-import pandas as pd
-
-from typing import List, Tuple
-
-def add(a: int, b: int) -> int:
-    """
-    Return the sum of 2 numbers.
-
-    :param a: first number
-    :param b: second number
-    :return: sum of 2 numbers
-    """
-    print("Inside function add()")
-    return a + b
-
-
-result = add(2, 3)
-
-print("Result: ", result)
-print("Outside function")'''
         # Run.
         actual = lareemli.update_function_blocks(text)
         # Check.
-        self.assertEqual(expected, actual)
+        test_output_dir = self.get_output_dir()
+        output_file_path = os.path.join(test_output_dir, "test.txt")
+        expected = hio.from_file(output_file_path)
+        self.assertEqual(expected, "\n".join(actual))
