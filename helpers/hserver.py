e9fa42f5ef35bfbaa4d0b3fe6f571266f124a03a
Heanh Sok <heanhsok@gmail.com>
2025-04-15 20:40:37 +0000

    checkpoint

--- helpers/hserver.py
+++ helpers/hserver.py
@@ -1081,7 +1081,6 @@ def config_func_to_str() -> str:
         "is_inside_docker",
         "is_inside_ecs_container",
         "is_inside_unit_test",
-        "is_mac",
         "is_prod_csfy",
         "run_docker_as_root",
         "skip_submodules_test",
