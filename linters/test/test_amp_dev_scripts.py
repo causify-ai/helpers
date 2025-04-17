6704efd38d60b9ed0c5340e3b4638d5b24c253fb
Allen Mathews <amathews699@gmail.com>
2025-04-17 06:43:44 -0400

    Added todo and skip markers back

--- linters/test/test_amp_dev_scripts.py
+++ linters/test/test_amp_dev_scripts.py
@@ -98,7 +98,11 @@ class Test_linter_py1(hunitest.TestCase):
 
     # #########################################################################
 
+    # TODO(heanh): Remove the skip when the dockerized executable issue is resolved.
     @pytest.mark.slow("About 6 sec")
+    @pytest.mark.skip(
+        "Skip due to issue related to dockerized executable. See HelpersTask553."
+    )
     def test_linter_md1(self) -> None:
         """
         Run Linter as executable on Markdown.
@@ -126,7 +130,11 @@ class Test_linter_py1(hunitest.TestCase):
         # Check.
         self.check_string(output, purify_text=True)
 
+    # TODO(heanh): Remove the skip when the dockerized executable issue is resolved.
     @pytest.mark.slow("About 6 sec")
+    @pytest.mark.skip(
+        "Skip due to issue related to dockerized executable. See HelpersTask553."
+    )
     def test_linter_md2(self) -> None:
         """
         Run Linter as executable on Markdown file with a fenced block.
