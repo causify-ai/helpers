48f52e9e151adf405c37852539bef42d392953d6
shaunak01 <shaunakdhande4000@gmail.com>
2025-04-22 18:08:58 +0000

    checkpoint

--- dev_scripts_helpers/update_devops_packages/notebooks/Master_buildmeister_dashboard.py
+++ dev_scripts_helpers/update_devops_packages/notebooks/Master_buildmeister_dashboard.py
@@ -3,33 +3,28 @@
 #   jupytext:
 #     text_representation:
 #       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.15.2
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.7
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
-# %% [markdown]
 #  TODO(Grisha): does it belong to the `devops` dir?
 
-# %% [markdown]
 # # Description
 
-# %% [markdown]
 # The notebook reports the latest build status for multiple repos.
 
-# %% [markdown]
 # # Imports
 
-# %%
 # %load_ext autoreload
 # %autoreload 2
 # %matplotlib inline
 
-# %%
+# +
 import logging
 from typing import Dict
 
@@ -41,23 +36,21 @@ import helpers.henv as henv
 import helpers.hpandas as hpandas
 import helpers.hprint as hprint
 import helpers.lib_tasks_gh as hlitagh
+# -
 
-# %%
 hdbg.init_logger(verbosity=logging.INFO)
 _LOG = logging.getLogger(__name__)
 _LOG.info("%s", henv.get_system_signature()[0])
 hprint.config_notebook()
 
-# %%
 # Set the display options to print the full table.
 pd.set_option("display.max_colwidth", None)
 pd.set_option("display.max_columns", None)
 
-# %% [markdown]
 # # Utils
 
 
-# %%
+# +
 def make_clickable(url: str) -> str:
     """
     Wrapper to make the URL value clickable.
@@ -88,10 +81,10 @@ def color_format(val: str, status_color_mapping: Dict[str, str]) -> str:
     return f"background-color: {color}"
 
 
-# %% [markdown]
+# -
+
 # # GH workflows state
 
-# %%
 repo_list = [
     "cryptokaizen/cmamp",
     "cryptokaizen/orange",
@@ -106,7 +99,6 @@ workflow_df = workflow_df[columns_order]
 workflow_df["url"] = workflow_df["url"].apply(make_clickable)
 _LOG.info(hpandas.df_to_str(workflow_df, log_level=logging.INFO))
 
-# %%
 status_color_mapping = {
     "success": "green",
     "failure": "red",
@@ -129,24 +121,23 @@ for repo in repos:
         )
     )
 
-# %% [markdown]
 # # Allure reports
 
-# %% [markdown]
 # - fast tests: http://172.30.2.44/allure_reports/cmamp/fast/latest/index.html
 # - slow tests: http://172.30.2.44/allure_reports/cmamp/slow/latest/index.html
 # - superslow tests: http://172.30.2.44/allure_reports/cmamp/superslow/latest/index.html
 
-# %% [markdown]
 # # Number of open pull requests
 
-# %%
 for repo in repo_list:
     number_prs = len(hlitagh.gh_get_open_prs(repo))
     _LOG.info("%s: %s", repo, number_prs)
 
-# %% [markdown]
 # # Code coverage HTML-page
 
-# %% [markdown]
 # http://172.30.2.44/html_coverage/runner_master/
+
+# # Code Coverage Page - CodeCov
+
+# - Helpers: https://app.codecov.io/gh/causify-ai/helpers
+# - Other Repos: https://app.codecov.io/gh/causify-ai/<repo_name>
