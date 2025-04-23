ae953b14adbfdbca6f1851af8305097f0645f95f
Heanh Sok <heanhsok@gmail.com>
2025-04-23 00:09:26 +0000

    Checkpoint
    Pre-commit checks:
    - 'check_master' passed
    - 'check_author' passed
    - 'check_file_size' passed
    - 'check_words' passed
    - 'check_python_compile' passed
    - 'check_gitleaks' passed
    All checks passed ✅

--- dev_scripts_helpers/git/git_hooks/pre-commit.py
+++ dev_scripts_helpers/git/git_hooks/pre-commit.py
@@ -17,6 +17,8 @@ import dev_scripts_helpers.git.git_hooks.pre-commit as dsgghpr
 # NOTE: This file should depend only on Python standard libraries.
 import logging
 import sys
+import pathlib
+from typing import List
 
 import dev_scripts_helpers.git.git_hooks.utils as dshgghout
 
@@ -25,19 +27,40 @@ _LOG = logging.getLogger(__name__)
 
 # #############################################################################
 
+def _write_output_to_file(lines: List[str]) -> None:
+    out_path = pathlib.Path(".git/hooks/tmp.precommit_output.txt")
+    with out_path.open("w") as f:
+        for line in lines:
+            f.write(line + "\n")
 
 if __name__ == "__main__":
     print("# Running git pre-commit hook ...")
+    lines = []
+    lines.append("Pre-commit checks:")
+    #
     dshgghout.check_master()
+    lines.append("- 'check_master' passed")
+    #
     dshgghout.check_author()
+    lines.append("- 'check_author' passed")
+    #
     dshgghout.check_file_size()
+    lines.append("- 'check_file_size' passed")
+    #
     dshgghout.check_words()
+    lines.append("- 'check_words' passed")
+    #
     dshgghout.check_python_compile()
+    lines.append("- 'check_python_compile' passed")
+    #
     dshgghout.check_gitleaks()
+    lines.append("- 'check_gitleaks' passed")
     print(
         "\n"
         + dshgghout.color_highlight(
             "##### All pre-commit hooks passed: committing ######", "purple"
         )
     )
+    lines.append("All checks passed ✅")
+    _write_output_to_file(lines)
     sys.exit(0)
