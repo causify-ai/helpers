1781088b5271f9d7937d2668e4ba0885f04cfd4f
Sandeep Thalapanane <ssandy086@gmail.com>
2025-05-01 12:51:33 -0400

    HelpersTask615: Propogate changes from previous commit
    Pre-commit checks:
    - 'check_master' passed
    - 'check_author' passed
    - 'check_file_size' passed
    - 'check_python_compile' passed
    - 'check_gitleaks' passed
    All checks passed ✅

--- helpers/test/test_lib_tasks_docker_release.py
+++ helpers/test/test_lib_tasks_docker_release.py
@@ -1,9 +1,7 @@
 import logging
-import re
 import unittest.mock as umock
-from typing import List, Tuple
+from typing import List
 
-import helpers.hprint as hprint
 import helpers.hsystem as hsystem
 import helpers.hunit_test as hunitest
 import helpers.lib_tasks_docker_release as hltadore
@@ -12,51 +10,28 @@ import helpers.test.test_lib_tasks as httestlib
 _LOG = logging.getLogger(__name__)
 
 
-def _normalize_command(cmd: str) -> str:
-    """
-    Normalize a command string by removing line continuations and spaces around
-    special characters.
-
-    :param cmd: command string to normalize
-    :return: normalized command string
-    """
-    cmd = re.sub(r"\\\s*\n\s*", " ", cmd)
-    chars_to_normalize = ["=", ",", "|"]
-    escaped_chars = "".join(re.escape(ch) for ch in chars_to_normalize)
-    cmd = re.sub(rf"\s*([{escaped_chars}])\s*", r"\1", cmd)
-    return cmd
-
-
-def _convert_commands_to_strings(
-    actual_cmds: List[str], expected_cmds: List[str]
-) -> Tuple[str, str]:
-    """
-    Convert a list of commands to strings and normalize them.
-
-    :param actual_cmds: list of actual command strings
-    :param expected_cmds: list of expected command strings
-    :return: normalized actual commands string and normalized expected
-        commands string
-    """
-    # Normalize each command in both lists.
-    actual_normalized = [_normalize_command(cmd) for cmd in actual_cmds]
-    expected_normalized = [_normalize_command(cmd) for cmd in expected_cmds]
-    # Convert to strings.
-    actual_str = "\n".join(actual_normalized)
-    expected_str = "\n".join(expected_normalized)
-    return actual_str, expected_str
-
-
 def _extract_commands_from_call(calls: List[umock._Call]) -> List[str]:
     """
     Extract command strings from a list of mock call arguments.
+
+    Example:
+        calls = [
+            (
+                # args tuple: (context, command)
+                (mock_ctx, "docker build --no-cache image1"),
+                # kwargs dictionary
+                {"pty": True}
+            )
+        ]
+        After extraction:
+        ["docker build --no-cache image1"]
 
     :param calls: list of mock call objects containing (args, kwargs)
     :return: list of command strings
     """
     # Each mock call is a (args, kwargs) tuple, extract the command string
     # from args[1] in each call.
-    call_list = [call[0][1] for call in calls]
+    call_list = [args_[1] for args_, kwargs_ in calls]
     return call_list
 
 
@@ -171,63 +146,36 @@ class TestDockerBuildLocalImage1(hunitest.TestCase):
             multi_arch=test_multi_arch,
         )
         actual_cmds = _extract_commands_from_call(self.mock_run.call_args_list)
-        # Check buildx setup and build commands.
-        actual_build_cmds = actual_cmds[:3]
-        expected_build_cmds = [
-            "cp -f devops/docker_build/dockerignore.dev .dockerignore",
-            "docker buildx create --name multiarch_builder --driver docker-container --bootstrap && docker buildx use multiarch_builder",
-            "tar -czh . | DOCKER_BUILDKIT=0 time docker buildx build "
-            " --no-cache "
-            " --push --platform linux/amd64,linux/arm64 "
-            " --build-arg AM_CONTAINER_VERSION=1.0.0 "
-            " --build-arg INSTALL_DIND=True "
-            " --build-arg POETRY_MODE=update "
-            " --build-arg CLEAN_UP_INSTALLATION=True "
-            f" --tag {test_base_image}:local-{self.user}-1.0.0 "
-            " --file devops/docker_build/dev.Dockerfile -",
-        ]
-        actual_build, expected_build = _convert_commands_to_strings(
-            actual_build_cmds, expected_build_cmds
-        )
+        actual_cmds = "\n".join(actual_cmds)
+        # The output is a list of strings, each representing a command.
+        exp = r"""
+        cp -f devops/docker_build/dockerignore.dev .dockerignore
+        docker buildx create \
+            --name multiarch_builder \
+            --driver docker-container \
+            --bootstrap \
+            && \
+            docker buildx use multiarch_builder
+        tar -czh . | DOCKER_BUILDKIT=0 \
+            time \
+            docker buildx build \
+            --no-cache \
+            --push \
+            --platform linux/amd64,linux/arm64 \
+            --build-arg AM_CONTAINER_VERSION=1.0.0 --build-arg INSTALL_DIND=True --build-arg POETRY_MODE=update --build-arg CLEAN_UP_INSTALLATION=True \
+            --tag test-registry.com/test-image:local-$USER_NAME-1.0.0 \
+            --file devops/docker_build/dev.Dockerfile \
+            -
+        docker pull test-registry.com/test-image:local-$USER_NAME-1.0.0
+        invoke docker_cmd --stage local --version 1.0.0 --cmd 'cp -f /install/poetry.lock.out /install/pip_list.txt .' --skip-pull
+        cp -f poetry.lock.out ./devops/docker_build/poetry.lock
+        cp -f pip_list.txt ./devops/docker_build/pip_list.txt
+        docker image ls test-registry.com/test-image:local-$USER_NAME-1.0.0
+        """
         self.assert_equal(
-            actual_build,
-            expected_build,
-            fuzzy_match=True,
-            remove_lead_trail_empty_lines=True,
-            dedent=True,
-        )
-        # Check pull and poetry file commands.
-        actual_poetry_cmds = actual_cmds[3:7]
-        expected_poetry_cmds = [
-            f"docker pull {test_base_image}:local-{self.user}-1.0.0",
-            "invoke docker_cmd  "
-            "--stage local "
-            f"--version {test_version} "
-            f"--cmd 'cp -f /install/poetry.lock.out /install/pip_list.txt .' --skip-pull",
-            "cp -f poetry.lock.out ./devops/docker_build/poetry.lock",
-            "cp -f pip_list.txt ./devops/docker_build/pip_list.txt",
-        ]
-        actual_poetry, expected_poetry = _convert_commands_to_strings(
-            actual_poetry_cmds, expected_poetry_cmds
-        )
-        self.assert_equal(
-            actual_poetry,
-            expected_poetry,
-            fuzzy_match=True,
-            remove_lead_trail_empty_lines=True,
-            dedent=True,
-        )
-        # Check list images command.
-        actual_list_cmd = actual_cmds[7]
-        expected_list_cmd = (
-            f"docker image ls {test_base_image}:local-{self.user}-1.0.0"
-        )
-        actual_list, expected_list = _convert_commands_to_strings(
-            actual_list_cmd, expected_list_cmd
-        )
-        self.assert_equal(
-            actual_list,
-            expected_list,
+            actual_cmds,
+            exp,
+            purify_text=True,
             fuzzy_match=True,
             remove_lead_trail_empty_lines=True,
             dedent=True,
