068e0bccc01c1b6469149fd14fa06259e2e63562
GP Saggese <saggese@gmail.com>
2025-05-02 13:52:03 -0400

    Fix
    Pre-commit checks:
    - 'check_master' passed
    - 'check_author' passed
    - 'check_file_size' passed
    - 'check_python_compile' passed
    - 'check_gitleaks' skipped
    All checks passed ✅

--- helpers/hjoblib.py
+++ helpers/hjoblib.py
@@ -450,7 +450,8 @@ def processify(func):
         p.join()
         if error:
             ex_type, ex_value, tb_str = error
-            message = "%s (in subprocess)\n%s" % (ex_value.message, tb_str)
+            #message = "%s (in subprocess)\n%s" % (ex_value.message, tb_str)
+            message = "%s (in subprocess)\n%s" % (str(ex_value), tb_str)
             raise ex_type(message)
         return ret
 
