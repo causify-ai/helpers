ac9f6f124f9cb837b203baeba4b84d3d55545508
Heanh Sok <heanhsok@gmail.com>
2025-05-05 21:25:31 +0000

    Checkpoint
    Pre-commit checks:
    - 'check_master' passed
    - 'check_author' passed
    - 'check_file_size' passed
    - 'check_python_compile' passed
    - 'check_gitleaks' passed
    All checks passed ✅

--- dev_scripts_helpers/thin_client/tmux.py
+++ dev_scripts_helpers/thin_client/tmux.py
@@ -6,8 +6,12 @@ Check whether a tmux session exists and, if not, creates it.
 import logging
 import os
 
+# This can be imported because this module is in the same dir as the script
+# being executed.
 import thin_client_utils as tcu
 
+# The `tcu` module adds root of helpers (or `helpers_root` when used in as
+# module) to the path, thus allowing imports from helpers.
 import helpers.hgit as hgit
 import helpers.repo_config_utils as hrecouti
 
@@ -16,10 +20,14 @@ _LOG = logging.getLogger(__name__)
 # Get the real file path rather than the symlink path.
 current_file_path = os.path.realpath(__file__)
 current_dir = os.path.dirname(current_file_path)
-# Change to the repo directory so that it can find the repo config.
+# Change to the directory where the file is located so it can find its way to
+# the Git root.
+# This is necessary when the script (e.g., `~/go_cmamp.py`) is symlinked and
+# executed from a different directory.
 os.chdir(current_dir)
 
-# Change to the outermost Git repository root.
+# Change to the outermost Git repository root so that it can find the repo
+# config.
 git_root_dir = hgit.find_git_root()
 os.chdir(git_root_dir)
 
