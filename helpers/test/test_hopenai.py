fd882569a12710549b64ce023c4d54959e560d9b
srinivassaitangudu <157262280+srinivassaitangudu@users.noreply.github.com>
2025-05-12 17:38:18 -0400

    Made sure no live API calls in tests.
    Pre-commit checks:
    - 'check_master' passed
    - 'check_author' passed
    - 'check_file_size' passed
    - 'check_python_compile' passed
    - 'check_gitleaks' passed
    All checks passed ✅

--- helpers/test/test_hopenai.py
+++ helpers/test/test_hopenai.py
@@ -7,11 +7,14 @@ from typing import Any, Dict
 
 import pandas as pd
 import pytest
+import unittest.mock as umock
 
 import helpers.hopenai as hopenai
 import helpers.hunit_test as hunitest
 
 _LOG = logging.getLogger(__name__)
+
+_TEST_CACHE_FILE = "test_get_completion_cache.json"
 
 _USER_PROMPT1 = "What is machine learning?"
 _USER_PROMPT2 = _USER_PROMPT1.lower()
@@ -27,10 +30,18 @@ _TEMPERATURE2 = 2
 _MODEL1="gpt-4o-mini"
 _MODEL2="gpt-o4-mini"
 
+def get_completion_paramters1():
+    data =  { 
+        "user_prompt": _USER_PROMPT1,
+        "system_prompt": _SYSTEM_PROMPT1,
+        "temperature":_TEMPERATURE1,
+        "model":_MODEL1
+    }
+    return data
 
 
-def get_request_parameters1():
 
+def get_openai_request_parameters1():
     messages = hopenai._construct_messages(user_prompt=_USER_PROMPT1, system_prompt=_SYSTEM_PROMPT1)
     data ={
         "messages":messages,
@@ -38,7 +49,16 @@ def get_request_parameters1():
         "model" :_MODEL1}
     return data
 
-def get_request_parameters2():
+def get_completion_paramters2():
+    data =  { 
+        "user_prompt": _USER_PROMPT2,
+        "system_prompt": _SYSTEM_PROMPT1,
+        "temperature":_TEMPERATURE1,
+        "model":_MODEL1
+    }
+    return data
+
+def get_openai_request_parameters2():
     messages = hopenai._construct_messages(user_prompt=_USER_PROMPT2, system_prompt=_SYSTEM_PROMPT1)
     data ={
         "messages":messages,
@@ -47,7 +67,16 @@ def get_request_parameters2():
 
     return data
 
-def get_request_parameters3():
+def get_completion_paramters3():
+    data =  { 
+        "user_prompt": _USER_PROMPT1,
+        "system_prompt": _SYSTEM_PROMPT1,
+        "temperature":_TEMPERATURE2,
+        "model":_MODEL1
+    }
+    return data
+
+def get_openai_request_parameters3():
     messages = hopenai._construct_messages(user_prompt=_USER_PROMPT1, system_prompt=_SYSTEM_PROMPT1)
     data ={
         "messages":messages,
@@ -55,7 +84,16 @@ def get_request_parameters3():
         "model" :_MODEL1}
     return data
 
-def get_request_parameters4():
+def get_completion_paramters4():
+    data =  { 
+        "user_prompt": _USER_PROMPT1,
+        "system_prompt": _SYSTEM_PROMPT1,
+        "temperature":_TEMPERATURE2,
+        "model":_MODEL2
+    }
+    return data
+
+def get_openai_request_parameters4():
     messages = hopenai._construct_messages(user_prompt=_USER_PROMPT1, system_prompt=_SYSTEM_PROMPT1)
     data ={
         "messages":messages,
@@ -75,7 +113,7 @@ def get_dummy_openai_response1():
                 "index": 0,
                 "message": {
                     "role": "assistant",
-                    "content": """ Machine learning is a subset of artificial intelligence
+                    "content": """Machine learning is a subset of artificial intelligence
                       (AI) that focuses on the development of algorithms and statistical models
                         that enable computers to perform tasks without explicit instructions. 
                         Instead, these systems learn from and make predictions or decisions based on data."""
@@ -121,21 +159,110 @@ def get_dummy_openai_response2():
         }
     }
 
+
+
+
+
+    
+############################################################################
+
+#############################################################################
+
+class BaseOpenAICacheTest(hunitest.TestCase):
+    """
+   
+    """
+    @pytest.fixture(autouse=True)
+    def setup_teardown_test(self):
+        #Patch get_completion to inject REPLAY
+        self.force_replay_cache()
+        # Run common setuo for each test
+        self.set_up_test()
+        yield
+        # Run common teardown after the test.
+        self.tear_down_test()
+
+
+    def force_replay_cache(self):
+        """
+        For all get_completion test cases, the cache_mode="REPLAY".
+        """
+        original_get_completion = hopenai.get_completion
+
+        def replay_get_completion(**kwargs):
+            return original_get_completion(**kwargs, cache_mode="REPLAY",cache_file=_TEST_CACHE_FILE )
+        
+        self.patcher =umock.patch.object(hopenai, "get_completion", replay_get_completion)
+        self.patcher.start()
+
+    def set_up_test(self)-> None:
+        """
+        Setup Operations to run before each test:
+        - adding dummy requests and responses in temporary cache file
+        """
+        
+        test_cache  = hopenai.OpenAICache(cache_file=_TEST_CACHE_FILE)
+
+        request_parameters1 = get_openai_request_parameters1()
+        request_parameters3 = get_openai_request_parameters3()
+
+        dummy_openai_response1 = get_dummy_openai_response1()
+        dummy_openai_response2 = get_dummy_openai_response2()
+
+        # generating hash keys
+        dummy_hash_key1 = test_cache.hash_key_generator(**request_parameters1)
+        dummy_hash_key2 = test_cache.hash_key_generator(**request_parameters3)
+
+        #saving dummy responses to cache
+        test_cache.save_response_to_cache(hash_key=dummy_hash_key1, request=request_parameters1, response=dummy_openai_response1)
+        test_cache.save_response_to_cache(hash_key=dummy_hash_key2, request=request_parameters3, response=dummy_openai_response2)
+
+    def tear_down_test(self):
+        """
+        Teardown operations to run after each test:
+            -  Remove cache files created on disk.
+        """
+        self.patcher.stop()
+        if os.path.exists(_TEST_CACHE_FILE):
+            os.remove(_TEST_CACHE_FILE)
+
+
+# #############################################################################
+# Test_get_completion
+# #############################################################################
+
+class Test_get_completion(BaseOpenAICacheTest):
+
+    def test1(self):
+        """
+        Verify that get_completion returns response from cache with the expected key and value.
+        """
+        
+
+        parameters1 = get_completion_paramters1()
+        print(parameters1)
+        dummy_response1= get_dummy_openai_response1()
+        response =hopenai.get_completion(**parameters1)
+
+        self.assertEqual(dummy_response1["choices"][0]["message"]["content"], response)
+
+
+
 # #############################################################################
 # OpenAICache's hash key generator tests
 # #############################################################################
 
 
-class Test_hash_key_generator(hunitest.TestCase):
+class Test_hash_key_generator(BaseOpenAICacheTest):
 
     def test_different_request_parameters1(self):
         """
         This test case checks if normalisation works before generating hash key
         """
-        parameters1= get_request_parameters1()
-        parameters2 = get_request_parameters2()
+        parameters1= get_openai_request_parameters1()
+        parameters2 = get_openai_request_parameters2()
 
-        openai_cache = hopenai.OpenAICache()
+        openai_cache = hopenai.OpenAICache(_TEST_CACHE_FILE)
         hash_key1= openai_cache.hash_key_generator(**parameters1)
         hash_key2 = openai_cache.hash_key_generator(**parameters2)
 
@@ -145,10 +272,10 @@ class Test_hash_key_generator(hunitest.TestCase):
         """
         Different Temperatures should give different hashkeys.
         """
-        parameters1= get_request_parameters1()
-        parameters3 = get_request_parameters3()
+        parameters1= get_openai_request_parameters1()
+        parameters3 = get_openai_request_parameters3()
 
-        openai_cache = hopenai.OpenAICache()
+        openai_cache = hopenai.OpenAICache(_TEST_CACHE_FILE)
         hash_key1= openai_cache.hash_key_generator(**parameters1)
         hash_key2 = openai_cache.hash_key_generator(**parameters3)
 
@@ -158,82 +285,17 @@ class Test_hash_key_generator(hunitest.TestCase):
         """
         Different models should give different hashkeys.
         """
-        parameters3= get_request_parameters3()
-        parameters4 = get_request_parameters4()
+        parameters3= get_openai_request_parameters3()
+        parameters4 = get_openai_request_parameters4()
 
-        openai_cache = hopenai.OpenAICache()
+        openai_cache = hopenai.OpenAICache(_TEST_CACHE_FILE)
         hash_key3= openai_cache.hash_key_generator(**parameters3)
         hash_key4 = openai_cache.hash_key_generator(**parameters4)
 
         self.assertNotEqual(hash_key3, hash_key4)
 
-
-
     
- ############################################################################
 
-#############################################################################
-
-class BaseOpenAICacheTest(hunitest.TestCase):
-    """
-    This pytest fixture ensures hopenai.get_completion uses replay mode while 
-    testing to prevent openai calls.
-    """
-    @pytest.fixture(autouse=True)
-    def setup_teardown_test(self, monkeypatch):
-        #Patch get_completion to inject REPLAY
-        self.force_replay_cache(monkeypatch)
-        # Run common setuo for each test
-        self.set_up_test()
-        yield
-        # Run common teardown after the test.
-        self.tear_down_test()
-
-
-    def force_replay_cache(self, monkeypatch):
-        """
-        For all get_completion test cases, the cache_mode="REPLAY".
-        """
-        original_get_completion = hopenai.get_completion
-
-        def replay_get_completion(**kwargs):
-            return original_get_completion(cache_mode="REPLAY", **kwargs)
-        
-        monkeypatch.setattr(hopenai, "get_completion", replay_get_completion)
-
-        yield
-
-    def set_up_test(self)-> None:
-        """
-        Setup Operations to run before each test:
-        - adding dummy requests and responses in temporary cache file
-        """
-        
-        test_cache_file = "test_get_completion_cache.json"
-        test_cache  = hopenai.OpenAICache(test_cache_file)
-
-        request_parameters1 = get_request_parameters1()
-        request_parameters2 = get_request_parameters2()
-
-        dummy_openai_response1 = get_dummy_openai_response1()
-        dummy_openai_response2 = get_dummy_openai_response2()
-
-        # generating hash keys
-        dummy_hash_key1 = test_cache.hash_key_generator(**request_parameters1)
-        dummy_hash_key2 = test_cache.hash_key_generator(**request_parameters2)
-
-        #saving dummy responses to cache
-        test_cache.save_respone_to_cache(hash_key=dummy_hash_key1, request=request_parameters1, response=dummy_openai_response1)
-        test_cache.save_respone_to_cache(hash_key=dummy_hash_key2, request=request_parameters2, response=dummy_openai_response2)
-
-    def tear_down_test(self):
-        """
-        Teardown operations to run after each test:
-            -  Remove cache files created on disk.
-        """
-        test_cache_file = "test_get_completion_cache.json"
-        if os.path.exists(test_cache_file):
-            os.remove(test_cache_file)
 
 
 
