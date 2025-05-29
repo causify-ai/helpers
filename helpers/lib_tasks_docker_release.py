1115235d15f8abaec4c57e8f35829c227c12bf91
Sandeep Thalapanane <ssandy086@gmail.com>
2025-05-29 13:45:19 -0700

    Merge master
    Pre-commit checks:
    All checks passed ✅

--- helpers/lib_tasks_docker_release.py
+++ helpers/lib_tasks_docker_release.py
@@ -1270,19 +1270,7 @@ def docker_create_candidate_image(ctx, user_tag=""):  # type: ignore
     )
     # Push candidate image.
     docker_push_prod_candidate_image(ctx, tag)
-<<<<<<< HEAD
-    exec_name = "datapull/aws/aws_update_task_definition.py"
-    # Ensure compatibility with repos where amp is a submodule.
-    if not hdbg.dassert_file_exists(exec_name):
-        exec_name = f"amp/{exec_name}"
-    hdbg.dassert_file_exists(exec_name)
-    _LOG.debug("exec_name=%s", exec_name)
-    # Register new task definition revision with updated image URL.
-    cmd = f'invoke docker_cmd -c "{exec_name} -t {task_definition} -i {tag} -r {region}"'
-    hlitauti.run(ctx, cmd)
-=======
     return tag
->>>>>>> origin/master
 
 
 # #############################################################################
