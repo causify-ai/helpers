8d38219b75028477cdbbfe70873b7458b11e168f
GP Saggese <saggese@gmail.com>
2025-06-08 17:14:52 -0400

    Improve
    Pre-commit checks:
    All checks passed ✅

--- papers/Causify_development_system/lint_latex.sh
+++ papers/Causify_development_system/lint_latex.sh
@@ -4,6 +4,6 @@ if [[ -z $GIT_ROOT ]]; then
     echo "Can't find GIT_ROOT=$GIT_ROOT"
     exit -1
 fi;
-FILE_NAME=$GIT_ROOT/papers/KaizenFlow/kaizen_flow.paper.tex
+FILE_NAME=$GIT_ROOT/papers/Causify_development_system/Causify_dev_system.tex
 
 dev_scripts/latex/lint_latex.sh $FILE_NAME
