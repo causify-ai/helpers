c374ec204bf1adac68501ebae18e944bc5b6d9e7
GP Saggese <saggese@gmail.com>
2025-07-06 14:05:04 -0400

    Merge
    Pre-commit checks:
    All checks passed ✅

--- dev_scripts_helpers/documentation/mkdocs/set_mkdocs.sh
+++ dev_scripts_helpers/documentation/mkdocs/set_mkdocs.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -xe
-<<<<<<< HEAD
-python3 -m venv mkdocs
-source mkdocs/bin/activate
-=======
 python3 -m venv mkdocs.venv
 source mkdocs.venv/bin/activate
->>>>>>> master
 pip install mkdocs-material
