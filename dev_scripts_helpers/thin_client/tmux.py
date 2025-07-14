5df5e16bc6065449047c8f937d91892a0d5aea4c
Heanh Sok <heanhsok@gmail.com>
2025-07-14 18:43:20 +0000

    Update
    Pre-commit checks:
    All checks passed ✅

--- dev_scripts_helpers/thin_client/tmux.py
+++ dev_scripts_helpers/thin_client/tmux.py
@@ -7,24 +7,10 @@ import logging
 import os
 import sys
 
-# Handle case where the script is not symlinked.
-# We assume the script is in `{repo_root}/dev_scripts_XYZ/thin_client/tmux.py`
-# so the repo root is 3 levels up.
-git_root = os.path.dirname(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-)
-# Check if `//helpers` is a submodule (e.g., `//cmamp`).
-if os.path.exists(os.path.join(git_root, "helpers_root")):
-    path = os.path.join(
-        git_root, "helpers_root", "dev_scripts_helpers", "thin_client"
-    )
-    sys.path.append(path)
-# Check if `//cmamp` is a submodule (e.g., `//orange`).
-if os.path.exists(os.path.join(git_root, "amp")):
-    path = os.path.join(
-        git_root, "amp", "helpers_root", "dev_scripts_helpers", "thin_client"
-    )
-    sys.path.append(path)
+real_path = os.path.realpath(__file__)
+dir_name = os.path.dirname(real_path)
+assert os.path.exists(os.path.join(dir_name, "thin_client_utils.py")), "Can't find thin_client_utils.py"
+sys.path.append(dir_name)
 
 # This can be imported because this module is in the same dir as the script
 # being executed.
