6c6d2979f386af5198e0bbdbad7b5fb87f544bb5
vlady <v.yakovenko@crypto-kaizen.com>
2025-08-29 16:00:34 +0000

    awscli

--- devops/docker_build/install_os_packages.sh
+++ devops/docker_build/install_os_packages.sh
@@ -57,7 +57,7 @@ if [[ 1 == 1 ]]; then
 fi;
 
 # - Install AWS CLI V2.
-if [[ 1 == 1 ]]; then
+if [[ -z "${AWS_CLI_INSTALL}" || "${AWS_CLI_INSTALL}" == "True" ]]; then
   # For more info see https://docs.aws.amazon.com/cli/latest/userguide/getting-started-version.html.
   # Changelog: https://raw.githubusercontent.com/aws/aws-cli/v2/CHANGELOG.rst.
   apt-get install $APT_GET_OPTS ca-certificates unzip curl
@@ -65,19 +65,21 @@ if [[ 1 == 1 ]]; then
   ARCH=$(uname -m)
   echo "ARCH=$ARCH"
   if [[ $ARCH == "x86_64" ]]; then
-      echo "Installing AWS CLI V2 for x86_64(Linux) architecture"
-      curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    echo "Installing AWS CLI V2 for x86_64(Linux) architecture"
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   elif [[ $ARCH == "aarch64" ]]; then
-      echo "Installing AWS CLI V2 for aarch64(Mac) architecture"
-      curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+    echo "Installing AWS CLI V2 for aarch64(Linux) architecture"
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
   else
-      echo "Unknown architecture $ARCH"
-      exit 1
+    echo "Unknown architecture $ARCH"
+    exit 1
   fi;
   unzip awscliv2.zip
   rm awscliv2.zip
   ./aws/install
   echo "AWS_CLI VERSION="$(aws --version)
+else
+  echo "Skipping AWS CLI V2 installation (AWS_CLI_INSTALL=${AWS_CLI_INSTALL})"
 fi;
 
 ## - Install Github CLI.
