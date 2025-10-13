921c4490fa2351b0fc81a9cf3747ee6b78cf3f7d
dremdem <dremdem@gmail.com>
2025-10-13 19:41:43 +0700

    add to tasks

--- tasks.py
+++ tasks.py
@@ -60,6 +60,7 @@ from helpers.lib_tasks import (  # isort: skip # noqa: F401  # pylint: disable=u
     find_test_decorator,
     fix_perms,
     gh_create_pr,
+    gh_issue_create,
     gh_issue_title,
     gh_login,
     gh_workflow_list,
