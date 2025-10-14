eed708af694935e1acae71098f41208f0d410feb
dremdem <dremdem@gmail.com>
2025-10-15 01:50:10 +0700

    Disable tests

--- helpers/lib_tasks_docker_release.py
+++ helpers/lib_tasks_docker_release.py
@@ -1722,8 +1722,10 @@ def docker_build_test_dev_image(  # type: ignore
         dev_version,
         skip_tests=False,
         fast_tests=True,
-        slow_tests=True,
-        superslow_tests=True,
+        slow_tests=False,
+        # slow_tests=True,
+        superslow_tests=False,
+        # superslow_tests=True,
         qa_tests=False,
     )
     # 6) Add changelog entry.
