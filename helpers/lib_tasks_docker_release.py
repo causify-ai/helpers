4e5b56fa5bea8f175142c4608d0c9a27ee45a2aa
dremdem <dremdem@gmail.com>
2025-11-14 23:20:51 +0700

    Branch naming changed for tesing

--- helpers/lib_tasks_docker_release.py
+++ helpers/lib_tasks_docker_release.py
@@ -1700,7 +1700,9 @@ def docker_build_test_dev_image(  # type: ignore
     _LOG.info("Step 3: Creating branch with date-based name")
     issue_prefix = hrecouti.get_repo_config().get_issue_prefix()
     # Get current date in YYYYMMDD format.
-    today = datetime.date.today().strftime("%Y%m%d")
+    # TODO(Vlad): For testing, need revert back before merging.
+    today = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    # today = datetime.date.today().strftime("%Y%m%d")
     branch_name = f"{issue_prefix}_Periodic_image_release_{today}"
     _LOG.info("Branch name: %s", branch_name)
     cmd = f"git checkout -b {branch_name}"
