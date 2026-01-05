4029601945b88e9e3d25b768fb50341a646b555c
Samarth <samarth.kapatel5@gmail.com>
2026-01-05 21:47:44 +0000

    Merge update

--- helpers/hllm.py
+++ helpers/hllm.py
@@ -539,7 +539,7 @@ _LLM_COST_Tracker = LLMCostTracker()
 def _call_api_sync(
     # pylint: disable=unused-argument
     # This is needed to support caching.
-    cache_mode: str,
+    # cache_mode: str,
     client: openai.OpenAI,
     user_prompt: str,
     system_prompt: str,
