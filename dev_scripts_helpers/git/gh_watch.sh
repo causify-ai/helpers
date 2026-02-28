361eada1fcabf204fef2c4d80b4996c5725b0f65
GP Saggese <33238329+gpsaggese@users.noreply.github.com>
2026-02-28 13:01:47 -0500

    gp_scratch_3 (#1157)
    * Update
    
    * Update

--- dev_scripts_helpers/git/gh_watch.sh
+++ dev_scripts_helpers/git/gh_watch.sh
@@ -3,11 +3,11 @@
 # Rename tmux pane to CC if inside tmux, and restore on exit.
 if [ -n "$TMUX" ]; then
     OLD_PANE_TITLE=$(tmux display-message -p '#W')
-    #echo "OLD_PANE_TITLE="$OLD_PANE_TITLE
-    tmux rename-window "*CC*"
+    echo "OLD_PANE_TITLE="$OLD_PANE_TITLE
+    tmux rename-window "*GH_WATCH*"
 fi
 
-claude --dangerously-skip-permissions
+watch -c -n 60 i gh_workflow_list
 
 if [ -n "$TMUX" ]; then
     tmux rename-window $OLD_PANE_TITLE
