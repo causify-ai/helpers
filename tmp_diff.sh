16b674537fb29429e4d77897df6197db22efcd91
GP Saggese <saggese@gmail.com>
2026-04-24 17:42:40 -0400

    Update

--- tmp_diff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ $1 == "wrap" ]]; then
-    cmd='vimdiff -c "windo set wrap"'
-else
-    cmd='vimdiff'
-fi;
-cmd="$cmd linters2/test/outcomes/Test_add_class_frame.test9/tmp.final.actual.txt linters2/test/outcomes/Test_add_class_frame.test9/tmp.final.expected.txt"
-eval $cmd
