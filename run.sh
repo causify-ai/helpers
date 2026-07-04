a5840c3cdc74d64018093a4c2ee77cdcf4dade83
GP Saggese <saggese@gmail.com>
2026-07-04 12:39:43 -0400

    Update

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
-export CSFY_DOCKER_ENGINE="docker"; i docker_cmd --stage=local -v 1.6.0 -cmd "pytest_log dev_scripts_helpers"
-export CSFY_DOCKER_ENGINE="docker"; pytest_log dev_scripts_helpers
-export CSFY_DOCKER_ENGINE="apple"; pytest_log dev_scripts_helpers
+# Delete cache.
+(export CSFY_DOCKER_ENGINE="docker"; i docker_cmd --stage=local -v 1.6.0 --cmd "pytest dev_scripts_helpers") 2>&1 | tee build1.txt
+# Delete cache.
+(export CSFY_DOCKER_ENGINE="docker"; pytest_log dev_scripts_helpers) 2>&1 | tee build2.txt
+# Delete cache.
+(export CSFY_DOCKER_ENGINE="apple"; pytest_log dev_scripts_helpers) 2>&1 | tee build3.txt
