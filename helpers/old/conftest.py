c8e3df4cb5d1fa8fa1bead72e7dab0fe12d48437
GP Saggese <saggese@gmail.com>
2026-08-16 13:17:04 -0400

    Update

--- helpers/old/conftest.py
+++ helpers/old/conftest.py
@@ -3,13 +3,12 @@ from typing import Any, Optional
 
 
 def pytest_ignore_collect(  # type: ignore
-    collection_path: pathlib.Path, path: Any, config: Any
+    collection_path: pathlib.Path, config: Any
 ) -> Optional[bool]:
     """
     Skip all tests in this directory.
 
     :param collection_path: path to analyze
-    :param path: path to analyze (deprecated)
     :param config: pytest config object
     :return: True if the path should be ignored
     """
